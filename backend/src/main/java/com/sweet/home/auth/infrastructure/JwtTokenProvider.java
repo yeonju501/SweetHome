a513a44e31594aa47501d90170c530274e4c9086
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-01-20 12:42:50 +0900

    [#4] feat: 토큰 생성 구현 및 테스트 추가

--- backend/src/main/java/com/sweet/home/auth/infrastructure/JwtTokenProvider.java
+++ backend/src/main/java/com/sweet/home/auth/infrastructure/JwtTokenProvider.java
@@ -1,13 +1,18 @@
 package com.sweet.home.auth.infrastructure;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import java.security.Key;
+import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JwtTokenProvider {
+
+    private static final String JWT_HEADER_PARAM_TYPE = "typ";
 
     private final Key key;
     private final String headerType;
@@ -23,5 +28,16 @@ public class JwtTokenProvider {
         this.headerType = headerType;
         this.issuer = issuer;
         this.accessTime = accessTime;
+    }
+
+    public String createToken(String subject) {
+        return Jwts.builder()
+            .signWith(key, SignatureAlgorithm.HS512)
+            .setHeaderParam(JWT_HEADER_PARAM_TYPE, headerType)
+            .setSubject(subject)
+            .setIssuer(issuer)
+            .setExpiration(new Date((new Date()).getTime() + accessTime))
+            .setIssuedAt(new Date())
+            .compact();
     }
 }
