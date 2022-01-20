a3cb1c0c0644b24a090d9b56f8bc333bdd1619d7
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-01-21 05:21:06 +0900

    [#6] feat: ProfileRestController GET /members/my-profile 추가

---
+++ backend/src/main/java/com/sweet/home/member/controller/ProfileRestController.java
@@ -0,0 +1,25 @@
+package com.sweet.home.member.controller;
+
+import com.sweet.home.member.controller.dto.response.ProfileResponse;
+import com.sweet.home.member.service.ProfileService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ProfileRestController {
+
+    private final ProfileService profileService;
+
+    public ProfileRestController(ProfileService profileService) {
+        this.profileService = profileService;
+    }
+
+    @GetMapping("/members/my-profile")
+    public ResponseEntity<ProfileResponse> viewMyProfile(@AuthenticationPrincipal String email) {
+        return ResponseEntity.ok().body(profileService.viewProfile(email));
+    }
+}
