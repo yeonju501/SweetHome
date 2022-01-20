7cd4608caca470d88d5395c51b474e9506bae811
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-01-20 22:50:13 +0900

    [#4] feat: ErrorCode enum 클래스 추가 및 authentication 에러 추가

---
+++ backend/src/main/java/com/sweet/home/global/exception/ErrorCode.java
@@ -0,0 +1,30 @@
+package com.sweet.home.global.exception;
+
+import org.springframework.http.HttpStatus;
+
+public enum ErrorCode {
+
+    // authentication
+    INVALID_EXPIRED_JWT(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
+    INVALID_MALFORMED_JWT(HttpStatus.UNAUTHORIZED, "잘못된 토큰 서명입니다."),
+    INVALID_UNSUPPORTED_JWT(HttpStatus.UNAUTHORIZED, "지원하지 않는 토큰입니다."),
+    INVALID_ILLEGAL_ARGUMENT_JWT(HttpStatus.UNAUTHORIZED, "토큰이 잘못되었습니다."),
+
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    ErrorCode(HttpStatus httpStatus, String message) {
+        this.httpStatus = httpStatus;
+        this.message = message;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
