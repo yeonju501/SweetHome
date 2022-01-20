c70309874bf8726c40d11c0758eaea7708eee8d7
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-01-21 05:59:48 +0900

    [#8] feat: Member 탈퇴 메소드 추가로 @Where 조회시 null 제외 추가

--- backend/src/main/java/com/sweet/home/member/domain/Member.java
+++ backend/src/main/java/com/sweet/home/member/domain/Member.java
@@ -13,12 +13,14 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import lombok.Builder;
 import lombok.Getter;
+import org.hibernate.annotations.Where;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Entity
 @Getter
+@Where(clause = "deleted_at is null")
 public class Member {
 
     @Id
