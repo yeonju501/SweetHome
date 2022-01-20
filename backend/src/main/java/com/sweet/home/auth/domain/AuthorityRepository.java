12066c68022038fbfbda094efcf1fd7c3641f9bd
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-01-21 03:25:59 +0900

    [#4] feat: Authority Entity 및 Repository 제거

--- backend/src/main/java/com/sweet/home/auth/domain/AuthorityRepository.java
+++
@@ -1,6 +0,0 @@
-package com.sweet.home.auth.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorityRepository extends JpaRepository<Authority, Long> {
-}
