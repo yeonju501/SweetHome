1f922586896a5a238bdfe5c3c00210252d416ecc
IcedCaffeLatte <tmdrl0160@naver.com>
2022-01-24 17:06:35 +0900

    [#10] feat: MessageContentRepository.java 추가

---
+++ backend/src/main/java/com/sweet/home/message/domain/MessageContentRepository.java
@@ -0,0 +1,6 @@
+package com.sweet.home.message.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MessageContentRepository extends JpaRepository<MessageContent, Long> {
+}
