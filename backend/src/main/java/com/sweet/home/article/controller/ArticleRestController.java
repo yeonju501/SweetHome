54a5d71c19696822d22130809886381ccfecbe22
yeonju501 <loling970124@gmail.com>
2022-01-24 18:37:11 +0900

    [#11] feat: 게시글 작성 api 구현

---
+++ backend/src/main/java/com/sweet/home/article/controller/ArticleRestController.java
@@ -0,0 +1,27 @@
+package com.sweet.home.article.controller;
+
+import com.sweet.home.article.controller.dto.request.ArticleSaveRequest;
+import com.sweet.home.article.service.ArticleService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api/boards")
+public class ArticleRestController {
+
+    private final ArticleService articleService;
+
+    public ArticleRestController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
+
+    @PostMapping("/articles")
+    public ResponseEntity<Void> create(@AuthenticationPrincipal String email, @RequestBody ArticleSaveRequest request) {
+        Long createMemberId = articleService.saveArticle(email, request);
+        URI uri = URI.create("/boards/articles/" + createMemberId);
+        return ResponseEntity.created(uri).build();
+    }
+}
--- backend/src/main/java/com/sweet/home/article/domain/Article.java
+++ backend/src/main/java/com/sweet/home/article/domain/Article.java
@@ -1,15 +1,18 @@
 package com.sweet.home.article.domain;
 
+import com.sweet.home.member.domain.Member;
+import lombok.Builder;
+import lombok.Getter;
+import org.hibernate.annotations.Where;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Where(clause = "deleted_at is null")
 public class Article {
 
     @Id
@@ -19,6 +22,14 @@ public class Article {
 
     @Column(name = "title", nullable = false)
     private String title;
+
+    @ManyToOne(targetEntity = Member.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+//    @ManyToOne(targetEntity = Board.class, fetch = FetchType.LAZY)
+//    @JoinColumn(name = "board_id")
+//    private Board board;
 
     @Column(name = "content", nullable = false)
     private String content;
@@ -34,7 +45,13 @@ public class Article {
     @Column(name = "deleted_at", nullable = true)
     private LocalDateTime deletedAt;
 
+    protected Article() {
+    }
 
-
-
+    @Builder
+    public Article(String title, String content, Member member){
+        this.title = title;
+        this.content = content;
+        this.member = member;
+    }
 }
