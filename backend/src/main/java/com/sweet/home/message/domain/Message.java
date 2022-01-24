4a6d7cc914661fc2b31721013755d34e841aae68
IcedCaffeLatte <tmdrl0160@naver.com>
2022-01-24 12:25:23 +0900

    [#10] fix: Message.java 다대일 선언 안 한 거 수정

--- backend/src/main/java/com/sweet/home/message/domain/Message.java
+++ backend/src/main/java/com/sweet/home/message/domain/Message.java
@@ -1,33 +1,50 @@
 package com.sweet.home.message.domain;
 
+import com.sweet.home.member.domain.Member;
+import lombok.Builder;
 import lombok.Getter;
+import org.hibernate.annotations.Where;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@Where(clause = "deleted_at is null")
 public class Message {
     @Id
     @GeneratedValue
     @Column(name = "message_id")
     private Long id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
     @Column(name = "message_content_id")
-    private Long messageContentId;
+    private MessageContent messageContentId;
 
+    @ManyToOne(fetch = FetchType.LAZY)
     @Column(name = "send_member_id")
-    private Long sendMemberId;
+    private Member sendMemberId;
 
+    @ManyToOne(fetch = FetchType.LAZY)
     @Column(name = "receive_member_id")
-    private Long receiveMemberId;
+    private Member receiveMemberId;
 
     @Column(name = "sender_receiver_delimiter")
     private SenderReceiverDelimiter senderReceiverDelimiter;
 
     @Column(name = "deleted_at", nullable = true)
     private LocalDateTime deletedAt;
+
+    public Message() {
+    }
+
+    @Builder
+    public Message(Long id, MessageContent messageContentId, Member sendMemberId, Member receiveMemberId, SenderReceiverDelimiter senderReceiverDelimiter, LocalDateTime deletedAt) {
+        this.id = id;
+        this.messageContentId = messageContentId;
+        this.sendMemberId = sendMemberId;
+        this.receiveMemberId = receiveMemberId;
+        this.senderReceiverDelimiter = senderReceiverDelimiter;
+        this.deletedAt = deletedAt;
+    }
 }
