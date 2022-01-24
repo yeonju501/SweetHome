bdbf2f6327eeecb7f393440b76625340e62e3166
IcedCaffeLatte <tmdrl0160@naver.com>
2022-01-24 17:07:51 +0900

    [#10] feat: MessageSendRequest.java 내부 구현

--- backend/src/main/java/com/sweet/home/message/controller/dto/request/MessageSendRequest.java
+++ backend/src/main/java/com/sweet/home/message/controller/dto/request/MessageSendRequest.java
@@ -1,4 +1,37 @@
 package com.sweet.home.message.controller.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.sweet.home.member.domain.Member;
+import com.sweet.home.message.domain.Message;
+import com.sweet.home.message.domain.MessageContent;
+import lombok.Getter;
+
+@Getter
 public class MessageSendRequest {
+
+    @JsonProperty("sender")
+    private String sender;
+
+    @JsonProperty("receiver")
+    private String receiver;
+
+    @JsonProperty("title")
+    private String title;
+
+    @JsonProperty("content")
+    private String content;
+
+    public MessageSendRequest() {
+    }
+
+    public MessageSendRequest(String sender, String receiver, String title, String content) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.title = title;
+        this.content = content;
+    }
+
+    public MessageContent toCreateMessageContent() {
+        return MessageContent.createMessageContent(title, content);
+    }
 }
