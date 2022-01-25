597a2bf5ea9855cb03f50999bb15293c40e12b34
yeonju501 <loling970124@gmail.com>
2022-01-25 17:49:09 +0900

    [#12] feat: BoardRestController에 showBoard() 추가

--- backend/src/main/java/com/sweet/home/board/controller/BoardRestController.java
+++ backend/src/main/java/com/sweet/home/board/controller/BoardRestController.java
@@ -1,8 +1,13 @@
 package com.sweet.home.board.controller;
 
+import com.sweet.home.board.controller.dto.response.BoardResponse;
 import com.sweet.home.board.service.BoardService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -10,7 +15,12 @@ public class BoardRestController {
 
     private final BoardService boardService;
 
-    public BoardRestController (BoardService boardService) {
+    public BoardRestController(BoardService boardService) {
         this.boardService = boardService;
+    }
+
+    @GetMapping("/boards")
+    public ResponseEntity<List<BoardResponse>> showBoard() {
+        return ResponseEntity.ok().body(boardService.findBoard());
     }
 }
