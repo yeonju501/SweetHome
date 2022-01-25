33294627a85573e9668939aa6315e0461d96f2f5
yeonju501 <loling970124@gmail.com>
2022-01-25 22:38:04 +0900

    [#20] feat: BoardRestController POST /boards 추가

--- backend/src/main/java/com/sweet/home/board/controller/BoardRestController.java
+++ backend/src/main/java/com/sweet/home/board/controller/BoardRestController.java
@@ -1,9 +1,13 @@
 package com.sweet.home.board.controller;
 
+import com.sweet.home.board.controller.dto.request.BoardSaveRequest;
 import com.sweet.home.board.controller.dto.response.BoardResponse;
 import com.sweet.home.board.service.BoardService;
+import java.net.URI;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +26,12 @@ public class BoardRestController {
     @GetMapping("/boards")
     public ResponseEntity<List<BoardResponse>> showBoard() {
         return ResponseEntity.ok().body(boardService.findAllBoards());
+    }
+
+    @PostMapping("/boards")
+    public ResponseEntity<Void> create(@RequestBody BoardSaveRequest request) {
+        boardService.saveBoard(request);
+        URI uri = URI.create("/api/boards");
+        return ResponseEntity.created(uri).build();
     }
 }
