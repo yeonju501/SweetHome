239495c64a8cb0a522824783b583259d28687e28
우동진 <wjinh93@gmail.com>
2022-01-25 15:34:25 +0900

    [#16] feat: Provider 추가

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import rootReducer from "./reducer/reducer";
+import { createStore } from "redux";
+import axios from "axios";
+
+const store = createStore(rootReducer);
+axios.defaults.withCredentials = true;
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>,
+
+	document.getElementById("root"),
 );
 
 // If you want to start measuring performance in your app, pass a function
