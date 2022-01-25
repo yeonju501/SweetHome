4ee4311dac720589248ac3e1250b101cb40c9bf0
yeonju501 <loling970124@gmail.com>
2022-01-25 17:49:26 +0900

    [#12] feat: BoardRestService에 findBoard() 추가

--- backend/src/main/java/com/sweet/home/board/service/BoardService.java
+++ backend/src/main/java/com/sweet/home/board/service/BoardService.java
@@ -1,14 +1,29 @@
 package com.sweet.home.board.service;
 
+import com.sweet.home.board.controller.dto.response.BoardResponse;
+import com.sweet.home.board.domain.Board;
 import com.sweet.home.board.domain.BoardRepository;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class BoardService {
 
     private final BoardRepository boardRepository;
 
-    public BoardService (BoardRepository boardRepository) {
+    public BoardService(BoardRepository boardRepository) {
         this.boardRepository = boardRepository;
+    }
+
+    @Transactional
+    public List<BoardResponse> findBoard() {
+        List<Board> boardList = boardRepository.findAll();
+        return boardList
+            .stream()
+            .map(BoardResponse::from)
+            .collect(Collectors.toList());
     }
 }
