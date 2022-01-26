e10d4ef206cd34a2cacb9ba821aa4f9578cfe530
yeonju501 <loling970124@gmail.com>
2022-01-26 14:38:03 +0900

    [#26] feat: ArticleResponse 추가

---
+++ backend/src/main/java/com/sweet/home/article/controller/dto/response/ArticleResponse.java
@@ -0,0 +1,46 @@
+package com.sweet.home.article.controller.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.sweet.home.article.domain.Article;
+import java.time.LocalDateTime;
+import lombok.Getter;
+
+@Getter
+public class ArticleResponse {
+
+    @JsonProperty("id")
+    private Long id;
+
+    @JsonProperty("title")
+    private String title;
+
+    @JsonProperty("member")
+    private MemberArticleResponse member;
+
+    @JsonProperty("content")
+    private String content;
+
+    @JsonProperty("created_at")
+    private LocalDateTime createdAt;
+
+    protected ArticleResponse() {
+    }
+
+    public ArticleResponse(Long id, String title, MemberArticleResponse member, String content, LocalDateTime createdAt) {
+        this.id = id;
+        this.title = title;
+        this.member = member;
+        this.content = content;
+        this.createdAt = createdAt;
+    }
+
+    public static ArticleResponse from(Article article, MemberArticleResponse member) {
+        return new ArticleResponse(
+            article.getId(),
+            article.getTitle(),
+            member,
+            article.getContent(),
+            article.getCreatedAt()
+        );
+    }
+}
