73a7bf42f891b7e166423cf0e3fd406969c75f81
우동진 <wjinh93@gmail.com>
2022-01-27 21:53:58 +0900

    [#37] refactor: SingInValid 함수 생성

--- frontend/src/utils/inputValid.js
+++ frontend/src/utils/inputValid.js
@@ -6,3 +6,11 @@ export function SignUpValid(email, password, phone_number) {
 
 	return regEmail.test(email) && regPassword.test(password) && regNumber.test(phone_number);
 }
+
+export function SignInValid(email, password) {
+	const regEmail =
+		/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
+	const regPassword = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{5,20}$/;
+	const isValidation = regEmail.test(email) && regPassword.test(password);
+	return isValidation;
+}
