22b2c59ea3a1e3d59d4c687f1825245b28482281
우동진 <wjinh93@gmail.com>
2022-01-27 21:53:43 +0900

    [#37] refactor: SingUpValid 함수 생성

---
+++ frontend/src/utils/inputValid.js
@@ -0,0 +1,8 @@
+export function SignUpValid(email, password, phone_number) {
+	const regEmail =
+		/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
+	const regNumber = /^01(?:0|1|[6-9])(?:\d{3}|\d{4})\d{4}$/;
+	const regPassword = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{5,20}$/;
+
+	return regEmail.test(email) && regPassword.test(password) && regNumber.test(phone_number);
+}
