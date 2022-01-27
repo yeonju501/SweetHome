43d717412bfd66c98c10e9df3d1b964c6ddb135b
우동진 <wjinh93@gmail.com>
2022-01-27 22:42:12 +0900

    [#37] refactor: Button 컴포넌트 생성

---
+++ frontend/src/components/accounts/SignButton.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import style from "../../style/SignIn.module.css";
+
+export function SignUpButton(props) {
+	return (
+		<div>
+			{props.valid ? (
+				<button className={style.login_button}>가입</button>
+			) : (
+				<button className={`${style.login_button} ${style.disabled_button}`} disabled>
+					가입
+				</button>
+			)}
+		</div>
+	);
+}
+
+export function SignInButton(props) {
+	return (
+		<div>
+			{props.valid ? (
+				<button className={style.login_button}>로그인</button>
+			) : (
+				<button className={`${style.login_button} ${style.disabled_button}`} disabled>
+					로그인
+				</button>
+			)}
+		</div>
+	);
+}
