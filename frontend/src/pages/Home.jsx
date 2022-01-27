b03fcdd6e854302a0261c8361b9445096eb7082f
우동진 <wjinh93@gmail.com>
2022-01-27 17:24:04 +0900

    [#36] refactor: axios삭제

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,4 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import SignIn from "../components/SignIn";
@@ -7,26 +6,11 @@ import style from "../style/Home.module.css";
 import { ReactComponent as Apart } from "../assets/main_apartment.svg";
 function Home() {
 	const navigate = useNavigate();
-	const [currentUser, setCurrentUser] = useState();
 	const token = useSelector((state) => state.token.token);
-
-	useEffect(() => {
-		(async function fetchData() {
-			if (token) {
-				axios({
-					url: "http://localhost:8080/api/members/my-profile",
-					headers: { Authorization: `Bearer ${token}` },
-					method: "get",
-				}).then((res) => {
-					setCurrentUser(res.data);
-				});
-			}
-		})();
-	}, [token]);
 
 	return (
 		<>
-			{currentUser ? (
+			{token ? (
 				navigate("/main")
 			) : (
 				<div className={style.Home_div}>
