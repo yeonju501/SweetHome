426f62f6af4af134b64b58c14dbd4c210c219d16
우동진 <wjinh93@gmail.com>
2022-01-27 22:31:37 +0900

    [#37] refactor: password jsx생성및  폴더구조 변경

--- frontend/src/components/accounts/SignIn.jsx
+++ frontend/src/components/accounts/SignIn.jsx
@@ -3,9 +3,9 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useDispatch } from "react-redux";
-import { SET_TOKEN } from "../store/token";
-import style from "../style/SignIn.module.css";
-import * as inputValid from "../utils/inputValid";
+import { SET_TOKEN } from "../../store/token";
+import style from "../../style/SignIn.module.css";
+import * as inputValid from "../../utils/inputValid";
 
 function SignIn() {
 	const navigate = useNavigate();
