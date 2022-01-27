56b163b6073c07fd39d8547f54c5ab455baed09b
IcedCaffeLatte <tmdrl0160@naver.com>
2022-01-27 10:49:05 +0900

    [#28] feat: MessageRestController.java 에 getSendMessageList 추가

--- backend/src/main/java/com/sweet/home/message/controller/MessageRestController.java
+++ backend/src/main/java/com/sweet/home/message/controller/MessageRestController.java
@@ -2,7 +2,9 @@ package com.sweet.home.message.controller;
 
 import com.sweet.home.message.controller.dto.request.MessageSendRequest;
 import com.sweet.home.message.controller.dto.response.MessageDetailResponse;
+import com.sweet.home.message.controller.dto.response.MessageResponse;
 import com.sweet.home.message.service.MessageService;
+import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -37,5 +39,10 @@ public class MessageRestController {
     public ResponseEntity<MessageDetailResponse> getDetailMessage(@AuthenticationPrincipal String email,
         @PathVariable(value = "message_id") Long messageId) {
         return ResponseEntity.ok().body(messageService.viewMessageDetail(email, messageId));
+    }
+
+    @GetMapping("/messages/send")
+    public ResponseEntity<List<MessageResponse>> getSendMessageList(@AuthenticationPrincipal String email) {
+        return ResponseEntity.ok().body(messageService.viewSendMessageList(email));
     }
 }
