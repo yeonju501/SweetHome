a0e1295ee573a9caefb4140dc7238baf4355bcef
IcedCaffeLatte <tmdrl0160@naver.com>
2022-01-27 10:47:40 +0900

    [#28] feat: MessageRepository.java 에 findBySendMemberAndSenderReceiverDelimiter 추가

--- backend/src/main/java/com/sweet/home/message/domain/MessageRepository.java
+++ backend/src/main/java/com/sweet/home/message/domain/MessageRepository.java
@@ -1,7 +1,10 @@
 package com.sweet.home.message.domain;
 
+import com.sweet.home.member.domain.Member;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
+    List<Message> findBySendMemberAndSenderReceiverDelimiter(Member sendMember, SenderReceiverDelimiter senderReceiverDelimiter);
 }
