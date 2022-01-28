372613d4af5da7eb014dfc39b9e3154b328280b0
vtaco <woosj7129@naver.com>
2022-01-28 23:35:25 +0900

    [#30] feat: axios catch & 렌더링

--- frontend/src/components/messages/ReadReceiveMessage.jsx
+++ frontend/src/components/messages/ReadReceiveMessage.jsx
@@ -21,11 +21,15 @@ function ReadReceiveMessage() {
 			method: "GET",
 			url: "http://localhost:8080/api/messages/receive",
 			headers: { Authorization: `Bearer ${token}` },
-		}).then((res) => {
-			setReceiveMessageArray(res.data);
-			console.log("res:data", res.data);
-			console.log("저장된 값 확인", receiveMessageArray);
-		});
+		})
+			.then((res) => {
+				setReceiveMessageArray(res.data);
+				console.log("res:data", res.data);
+				console.log("저장된 값 확인", receiveMessageArray);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
 	});
 	return (
 		<div>
