87acb590823833ae781046de0a9a9dc7facd4551
vtaco <woosj7129@naver.com>
2022-01-28 22:09:15 +0900

    [#30] feat: getReceiveMessage 선언

--- frontend/src/components/messages/ReadReceiveMessage.jsx
+++ frontend/src/components/messages/ReadReceiveMessage.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 function ReadReceiveMessage() {
 	const token = window.localStorage.getItem("access_token");
@@ -13,7 +14,17 @@ function ReadReceiveMessage() {
 		read_at: "",
 	});
 
-
+	function getReceiveMessage(e) {
+		e.preventDefault();
+		axios({
+			method: "GET",
+			url: "http://localhost:8080/api/messages/send",
+			headers: { Authorization: `Bearer ${token}` },
+		}).then((res) => {
+			setReadReceiveMessage(res.data);
+			console.log(res.data);
+		});
+	}
 	return (
 		<div>
 			<h1>ReadReciveMessage</h1>
