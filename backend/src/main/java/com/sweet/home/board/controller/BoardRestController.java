9a9feea692b5c6effe867088c9ee6e67a89d2e3c
yeonju501 <loling970124@gmail.com>
2022-01-28 11:49:09 +0900

    [#34] feat: BoardRestController, Service addFavorite 추가

--- backend/src/main/java/com/sweet/home/board/controller/BoardRestController.java
+++ backend/src/main/java/com/sweet/home/board/controller/BoardRestController.java
@@ -1,10 +1,12 @@
 package com.sweet.home.board.controller;
 
+import com.sweet.home.board.controller.dto.request.BoardFavoriteRequest;
 import com.sweet.home.board.controller.dto.request.BoardSaveRequest;
 import com.sweet.home.board.controller.dto.response.BoardResponse;
 import com.sweet.home.board.service.BoardService;
 import java.net.URI;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,7 +26,7 @@ public class BoardRestController {
     }
 
     @GetMapping("/boards")
-    public ResponseEntity<List<BoardResponse>> showBoard() {
+    public ResponseEntity<List<BoardResponse>> showBoards() {
         return ResponseEntity.ok().body(boardService.findAllBoards());
     }
 
@@ -32,6 +34,13 @@ public class BoardRestController {
     public ResponseEntity<Void> createBoard(@RequestBody BoardSaveRequest request) {
         boardService.saveBoard(request);
         URI uri = URI.create("/api/boards");
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PostMapping("/boards/favorite")
+    public ResponseEntity<Void> addFavorite(@AuthenticationPrincipal String email, @RequestBody BoardFavoriteRequest request) {
+        boardService.addFavorite(email, request);
+        URI uri = URI.create("/api/boards/favorite");
         return ResponseEntity.created(uri).build();
     }
 }
