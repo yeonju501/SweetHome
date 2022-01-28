b27a80b98f4f3f7c7d77babe1fc32eebe61f105b
9603100 <87457698+9603100@users.noreply.github.com>
2022-01-28 10:41:45 +0900

    [#43] feat: 메인 화면 구성 초안

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import BoardList from "../components/BoardList";
 
 function Main() {
 	const access = useSelector((state) => state.token.token);
-	return <div>hi {access}</div>;
+	return (
+		<div>
+			hi {access}
+			<div>nav-bar</div>
+			<BoardList />
+			<div>글 목록</div>
+		</div>
+	);
 }
 
 export default Main;
