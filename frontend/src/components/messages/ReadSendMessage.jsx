dd1f63d496358f938074dab32ca83eb6f9aabf1f
vtaco <woosj7129@naver.com>
2022-01-29 00:21:10 +0900

    [#30] feat: useEffect axios 추가

--- frontend/src/components/messages/ReadSendMessage.jsx
+++ frontend/src/components/messages/ReadSendMessage.jsx
@@ -1,10 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
+import axios from "axios";
 
 function ReadSendMessage() {
 	const token = useSelector((state) => state.token.token);
-	const [receiveMessageArray, setReceiveMessageArray] = useState([]);
+	const [sendMessageArray, setSendMessageArray] = useState([]);
 
+	useEffect(() => {
+		axios({
+			method: "GET",
+			url: "http://localhost:8080/api/messages/send",
+			headers: { Authorization: `Bearer ${token}` },
+		})
+			.then((res) => {
+				setSendMessageArray(res.data);
+				console.log("저장된 값 확인", sendMessageArray);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, []);
 	return (
 		<div>
 			<h1>ReadSendMessage</h1>
