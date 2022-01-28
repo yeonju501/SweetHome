7912c6b8fd76b5171ca78f4c6881a241cbb2a3de
yeonju501 <loling970124@gmail.com>
2022-01-28 15:49:55 +0900

    [#34] feat: BoardFavoriteRepository findAllByMember 추가

--- backend/src/main/java/com/sweet/home/board/domain/BoardFavoriteRepository.java
+++ backend/src/main/java/com/sweet/home/board/domain/BoardFavoriteRepository.java
@@ -1,7 +1,10 @@
 package com.sweet.home.board.domain;
 
+import com.sweet.home.member.domain.Member;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardFavoriteRepository extends JpaRepository<BoardFavorite, Long> {
 
+    List<BoardFavorite> findAllByMember(Member member);
 }
