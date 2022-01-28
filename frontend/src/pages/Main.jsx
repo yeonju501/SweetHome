ae48e1633d00c45ab1886b20fa3efbffc602dfb6
우동진 <wjinh93@gmail.com>
2022-01-29 01:52:14 +0900

    [#45] feat: Main에서 Profile 이동 Link추가

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import BoardList from "../components/BoardList";
+import { Link } from "react-router-dom";
 
 function Main() {
-	const access = useSelector((state) => state.token.token);
+	const token = useSelector((state) => state.token.token);
+	const [userInfo, setUserInfo] = useState(null);
+
 	return (
-		<div>
-			hi {access}
-			<div>nav-bar</div>
-			<BoardList />
-			<div>글 목록</div>
-		</div>
+		userInfo && (
+			<div>
+				hi {token}
+				<div>nav-bar</div>
+				<BoardList />
+				<div>글 목록</div>
+				<p>{userInfo.username}</p>
+				<Link to={`/profile/${userInfo.username}`} state={{ user: userInfo }}>
+					Profile
+				</Link>
+			</div>
+		)
 	);
 }
 
