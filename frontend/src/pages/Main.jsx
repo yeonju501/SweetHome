251200240b64bb95b1a940e8e2d805fd6a7f85ef
우동진 <wjinh93@gmail.com>
2022-01-29 01:53:11 +0900

    [#45] feat: Main에서 유저정보 받아오기

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import BoardList from "../components/BoardList";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 function Main() {
 	const token = useSelector((state) => state.token.token);
 	const [userInfo, setUserInfo] = useState(null);
+
+	useEffect(() => {
+		axios({
+			url: "http://localhost:8080/api/members/my-profile",
+			method: "get",
+			headers: { Authorization: `Bearer ${token}` },
+		}).then((res) => setUserInfo(res.data));
+	}, []);
 
 	return (
 		userInfo && (
