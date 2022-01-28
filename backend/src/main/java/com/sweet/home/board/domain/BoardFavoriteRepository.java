7a89e0fb10c924e3683a1039e6b0da16014f60a2
yeonju501 <loling970124@gmail.com>
2022-01-28 11:47:26 +0900

    [#34] feat: BoardFavoriteRepository, Request 추가

---
+++ backend/src/main/java/com/sweet/home/board/domain/BoardFavoriteRepository.java
@@ -0,0 +1,7 @@
+package com.sweet.home.board.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardFavoriteRepository extends JpaRepository<BoardFavorite, Long> {
+
+}
