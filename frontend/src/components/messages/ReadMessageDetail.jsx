43393585d8d7d3daa1a8626ffeb31eba8b2d6b8e
vtaco <woosj7129@naver.com>
2022-01-29 16:10:48 +0900

    [#50] feat: axios 데이터 전송확인

--- frontend/src/components/messages/ReadMessageDetail.jsx
+++ frontend/src/components/messages/ReadMessageDetail.jsx
@@ -1,18 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
+import axios from "axios";
 
 function ReadMessageDeatil() {
 	const token = useSelector((state) => state.token.token);
 	const [messageDetail, setMessageDetail] = useState({
 		sender_username: "",
-		sender_email,"",
-    receiver_username:"",
-    receiver_email:"",
-    title:"",
-    content:"",
-    send_at:"",
-    read_at:"",
+		sender_email: "",
+		receiver_username: "",
+		receiver_email: "",
+		title: "",
+		content: "",
+		send_at: "",
+		read_at: "",
 	});
+
+	useEffect(() => {
+		axios({
+			method: "GET",
+			url: "http://localhost:8080/api/messages/1",
+			headers: { Authorization: `Bearer ${token}` },
+		})
+			.then((res) => {
+				console.log(res.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, []);
 	return (
 		<div>
 			<h1>ReadMessageDetail</h1>
