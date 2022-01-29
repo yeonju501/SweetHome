61b7f8318a8d1a8601d5c37f39e5b1684fa9ac9b
IcedCaffeLatte <tmdrl0160@naver.com>
2022-01-29 15:33:13 +0900

    [#42] feat: LoginMemberResponse 에 Refresh token 반환 추가

--- backend/src/main/java/com/sweet/home/auth/controller/dto/response/LoginMemberResponse.java
+++ backend/src/main/java/com/sweet/home/auth/controller/dto/response/LoginMemberResponse.java
@@ -1,6 +1,7 @@
 package com.sweet.home.auth.controller.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.sweet.home.auth.domain.Tokens;
 import lombok.Getter;
 
 @Getter
@@ -8,11 +9,14 @@ public class LoginMemberResponse {
 
     @JsonProperty("access_token")
     private String accessToken;
+    @JsonProperty("refresh_token")
+    private String refreshToken;
 
     public LoginMemberResponse() {
     }
 
-    public LoginMemberResponse(String accessToken) {
-        this.accessToken = accessToken;
+    public LoginMemberResponse(Tokens tokens) {
+        this.accessToken = tokens.getAccessToken();
+        this.refreshToken = tokens.getRefreshToken();
     }
 }
