5c2eef597904e12960a00820a87910181c53cf4c
vtaco <woosj7129@naver.com>
2022-01-29 15:45:04 +0900

    [#50] feat: token 생성

--- frontend/src/components/messages/ReadMessageDetail.jsx
+++ frontend/src/components/messages/ReadMessageDetail.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 function ReadMessageDeatil() {
+	const token = useSelector((state) => state.token.token);
+
 	return (
 		<div>
 			<h1>ReadMessageDetail</h1>
