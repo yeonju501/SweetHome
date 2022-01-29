8c2bcb5433466239f6c0a62671cffc57be9c4ae8
vtaco <woosj7129@naver.com>
2022-01-29 15:50:52 +0900

    [#50] feat: messageDtail useState

--- frontend/src/components/messages/ReadMessageDetail.jsx
+++ frontend/src/components/messages/ReadMessageDetail.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 
 function ReadMessageDeatil() {
 	const token = useSelector((state) => state.token.token);
-
+	const [messageDetail, setMessageDetail] = useState({
+		sender_username: "",
+		sender_email,"",
+    receiver_username:"",
+    receiver_email:"",
+    title:"",
+    content:"",
+    send_at:"",
+    read_at:"",
+	});
 	return (
 		<div>
 			<h1>ReadMessageDetail</h1>
