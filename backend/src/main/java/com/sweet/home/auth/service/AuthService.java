4b02ec6bd359915658a0cf7579fbbb1f1abd6f5c
IcedCaffeLatte <tmdrl0160@naver.com>
2022-01-29 15:37:33 +0900

    [#42] feat: AuthService.java 에서 로그인할 때 refreshToken 도 받는 기능 추가

--- backend/src/main/java/com/sweet/home/auth/service/AuthService.java
+++ backend/src/main/java/com/sweet/home/auth/service/AuthService.java
@@ -2,11 +2,15 @@ package com.sweet.home.auth.service;
 
 import com.sweet.home.auth.controller.dto.request.LoginRequest;
 import com.sweet.home.auth.controller.dto.response.LoginMemberResponse;
+import com.sweet.home.auth.domain.Tokens;
+import com.sweet.home.auth.domain.RefreshToken;
+import com.sweet.home.auth.domain.RefreshTokenRepository;
 import com.sweet.home.auth.infrastructure.JwtTokenProvider;
 import com.sweet.home.global.exception.BusinessException;
 import com.sweet.home.global.exception.ErrorCode;
 import com.sweet.home.member.domain.Member;
 import com.sweet.home.member.domain.MemberRepository;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,20 +21,31 @@ public class AuthService {
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtTokenProvider jwtTokenProvider;
+    private final RefreshTokenRepository refreshTokenRepository;
 
     public AuthService(MemberRepository memberRepository, PasswordEncoder passwordEncoder,
-        JwtTokenProvider jwtTokenProvider) {
+        JwtTokenProvider jwtTokenProvider, RefreshTokenRepository refreshTokenRepository) {
         this.memberRepository = memberRepository;
         this.passwordEncoder = passwordEncoder;
         this.jwtTokenProvider = jwtTokenProvider;
+        this.refreshTokenRepository = refreshTokenRepository;
     }
 
-    @Transactional(readOnly = true)
+    @Transactional
     public LoginMemberResponse login(LoginRequest request) {
         Member member = memberRepository.findByEmail(request.getEmail())
             .orElseThrow(() -> new BusinessException(ErrorCode.MEMBER_NOT_FOUND_BY_EMAIL));
         member.login(passwordEncoder, request.getPassword());
 
-        return new LoginMemberResponse(jwtTokenProvider.createToken(member.getEmail(), member.getAuthority()));
+        Tokens tokens = jwtTokenProvider.createToken(member.getEmail(), member.getAuthority());
+
+        RefreshToken refreshToken = RefreshToken.builder()
+            .key(member.getEmail())
+            .value(tokens.getRefreshToken())
+            .build();
+
+        refreshTokenRepository.save(refreshToken);
+
+        return new LoginMemberResponse(tokens);
     }
 }
