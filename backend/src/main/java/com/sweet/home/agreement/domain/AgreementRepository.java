0e096a62570aaa3c3fe9aefb10e66d848f0b28ee
IcedCaffeLatte <tmdrl0160@naver.com>
2022-01-30 21:13:00 +0900

    [#70] feat: AgreementRepository 에 findByBuilding 추가

--- backend/src/main/java/com/sweet/home/agreement/domain/AgreementRepository.java
+++ backend/src/main/java/com/sweet/home/agreement/domain/AgreementRepository.java
@@ -1,7 +1,9 @@
 package com.sweet.home.agreement.domain;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AgreementRepository extends JpaRepository<Agreement, Long> {
-
+    Page<Agreement> findByBuilding(String building, Pageable pageable);
 }
