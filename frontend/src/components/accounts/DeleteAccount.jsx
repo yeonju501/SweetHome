9ca73fbbeb3dada034d719b9900be16b75fe1de4
우동진 <wjinh93@gmail.com>
2022-01-30 16:04:09 +0900

    [#64] feat: 회원탈퇴시 토큰 삭제 및 홈 화면으로 이동

--- frontend/src/components/accounts/DeleteAccount.jsx
+++ frontend/src/components/accounts/DeleteAccount.jsx
@@ -1,11 +1,14 @@
 import axios from "axios";
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router";
+import { DELETE_TOKEN } from "../../store/token";
+
 function DeleteAccount() {
 	const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 	const token = useSelector((state) => state.token.token);
 	const navigate = useNavigate();
+	const dispatch = useDispatch();
 
 	const deleteAccount = () => {
 		window.confirm("정말로 회원 탈퇴를 진행 하시겠습니까?") &&
@@ -14,6 +17,7 @@ function DeleteAccount() {
 				method: "delete",
 				headers: { Authorization: `Bearer ${token}` },
 			})
+				.then(dispatch(DELETE_TOKEN()))
 				.then(navigate("/"))
 				.catch((err) => console.log(err));
 	};
