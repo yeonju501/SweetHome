be810cc2fb89ffaf1a42873460b921660e20ece1
vtaco <woosj7129@naver.com>
2022-01-30 21:44:29 +0900

    [#57] refactor: MessageBox.jsx Link 연결

--- frontend/src/pages/MessageBox.jsx
+++ frontend/src/pages/MessageBox.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { Link, Outlet } from "react-router-dom";
 
 function MessageBox() {
 	return (
 		<div>
 			<h1>MessageBox</h1>
+
+			<Link to="read-receive-message">ReadReceiveMessage</Link>
+
+			<Link to="read-send-message">ReadSendMessage</Link>
+
+			<Link to="send-message">SendMessage</Link>
+			<Outlet />
 		</div>
 	);
 }
