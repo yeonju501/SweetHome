37285966d3e0231b8221734ece53e66f7353e929
yeonju501 <loling970124@gmail.com>
2022-01-30 11:35:10 +0900

    [#58] feat: Article 좋아요 조회 기능 추가, api uri 변경

--- backend/src/main/java/com/sweet/home/article/controller/ArticleRestController.java
+++ backend/src/main/java/com/sweet/home/article/controller/ArticleRestController.java
@@ -38,26 +38,26 @@ public class ArticleRestController {
     }
 
     @GetMapping("/{boardId}/articles")
-    public ResponseEntity<List<ArticleTitleResponse>> showArticles(@PathVariable Long boardId, @PageableDefault Pageable pageable) {
+    public ResponseEntity<List<ArticleTitleResponse>> showArticles(@PathVariable Long boardId,
+        @PageableDefault Pageable pageable) {
         articleService.findAllByBoard(boardId, pageable);
         return ResponseEntity.ok().body(articleService.findAllByBoard(boardId, pageable));
     }
 
-    @GetMapping("/{boardId}/articles/{articleId}")
-    public ResponseEntity<ArticleDetailResponse> showArticle(@PathVariable Long boardId, @PathVariable Long articleId) {
+    @GetMapping("/articles/{articleId}")
+    public ResponseEntity<ArticleDetailResponse> showArticle(@PathVariable Long articleId) {
         return ResponseEntity.ok().body(articleService.showArticle(articleId));
     }
 
-    @PutMapping("/{boardId}/articles/{articleId}")
-    public ResponseEntity<Void> updateArticle(@AuthenticationPrincipal String email,
-        @RequestBody ArticleSaveRequest request, @PathVariable Long boardId, @PathVariable Long articleId) {
+    @PutMapping("/articles/{articleId}")
+    public ResponseEntity<Void> updateArticle(@AuthenticationPrincipal String email, @RequestBody ArticleSaveRequest request,
+        @PathVariable Long articleId) {
         articleService.updateArticle(email, request, articleId);
         return ResponseEntity.noContent().build();
     }
 
-    @DeleteMapping("/{boardId}/articles/{articleId}")
-    public ResponseEntity<Void> deleteArticle(@AuthenticationPrincipal String email, @PathVariable Long boardId,
-        @PathVariable Long articleId) {
+    @DeleteMapping("/articles/{articleId}")
+    public ResponseEntity<Void> deleteArticle(@AuthenticationPrincipal String email, @PathVariable Long articleId) {
         articleService.deleteArticle(email, articleId);
         return ResponseEntity.noContent().build();
     }
