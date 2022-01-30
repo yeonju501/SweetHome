2b2b397fdd10b95b9ae5a7c485ce3495710f4b21
yeonju501 <loling970124@gmail.com>
2022-01-30 23:42:09 +0900

    [#80] feat: 댓글 좋아요 추가 기능

---
+++ backend/src/main/java/com/sweet/home/comment/controller/CommentLikeRestController.java
@@ -0,0 +1,26 @@
+package com.sweet.home.comment.controller;
+
+import com.sweet.home.comment.service.CommentLikeService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/comments")
+public class CommentLikeRestController {
+
+    private final CommentLikeService commentLikeService;
+
+    public CommentLikeRestController(CommentLikeService commentLikeService) {
+        this.commentLikeService = commentLikeService;
+    }
+
+    @PostMapping("/{commentId}/likes")
+    public ResponseEntity<Void> likeComment(@AuthenticationPrincipal String email, @PathVariable Long commentId) {
+        commentLikeService.likeComment(email, commentId);
+        return ResponseEntity.noContent().build();
+    }
+}
