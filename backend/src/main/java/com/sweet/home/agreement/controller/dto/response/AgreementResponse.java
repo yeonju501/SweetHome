d92370cd5c694fb1a3b4c25bb47e69f48e5b819f
IcedCaffeLatte <tmdrl0160@naver.com>
2022-01-30 21:13:17 +0900

    [#70] feat: AgreementResponse 작성

--- backend/src/main/java/com/sweet/home/agreement/controller/dto/response/AgreementResponse.java
+++ backend/src/main/java/com/sweet/home/agreement/controller/dto/response/AgreementResponse.java
@@ -1,5 +1,44 @@
 package com.sweet.home.agreement.controller.dto.response;
 
-public class AgreementResponse {
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.sweet.home.agreement.domain.Agreement;
+import java.time.LocalDateTime;
 
+public class AgreementResponse {
+    @JsonProperty("title")
+    private String title;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    @JsonProperty("created_at")
+    private LocalDateTime createdAt;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    @JsonProperty("start_date")
+    private LocalDateTime startDate;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    @JsonProperty("end_date")
+    private LocalDateTime endDate;
+
+
+    public AgreementResponse() {
+    }
+
+    public AgreementResponse(String title, LocalDateTime createdAt, LocalDateTime startDate,
+        LocalDateTime endDate) {
+        this.title = title;
+        this.createdAt = createdAt;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public static AgreementResponse from(Agreement agreement){
+        return new AgreementResponse(
+            agreement.getTitle(),
+            agreement.getCreatedAt(),
+            agreement.getStartDate(),
+            agreement.getEndDate()
+        );
+    }
 }
