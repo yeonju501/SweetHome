a9ff946114edf4e06d677aec0478ef59c1eb6881
9603100 <87457698+9603100@users.noreply.github.com>
2022-01-30 20:33:58 +0900

    [#58] refactor: 게시판 목록 받는 위치 Main.jsx로 변경

--- frontend/src/components/BoardList.jsx
+++ frontend/src/components/BoardList.jsx
@@ -1,32 +1,8 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-
-const SERVER_URL = process.env.REACT_APP_SERVER_URL;
-
-function BoardList() {
-	const token = useSelector((state) => state.token.token);
-	const [boards, setBoards] = useState([]);
-
-	useEffect(() => {
-		axios({
-			url: `${SERVER_URL}/api/boards`,
-			headers: { Authorization: `Bearer ${token}` },
-			method: "get",
-		})
-			.then((res) => {
-				console.log(res);
-				setBoards(res.data);
-			})
-			.catch((err) => {
-				console.log(err);
-			});
-	}, []);
-
+function BoardList({ boards }) {
 	return (
 		<ul>
-			{boards.map((board, idx) => (
-				<li key={idx}>{board.name}</li>
+			{boards.map((board) => (
+				<li key={board.id}>{board.name}</li>
 			))}
 		</ul>
 	);
