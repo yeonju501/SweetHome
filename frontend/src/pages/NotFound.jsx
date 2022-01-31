e40c71b37d0c325dc94629ed97cf906b91a3444f
우동진 <wjinh93@gmail.com>
2022-01-31 15:30:22 +0900

    [#87] style: css 추가

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -3,9 +3,23 @@ import { Link } from "react-router-dom";
 
 function NotFound() {
 	return (
-		<div>
+		<div
+			style={{
+				display: "flex",
+				flexDirection: "column",
+				justifyContent: "center",
+				height: "100vh",
+				fontSize: "2rem",
+				textAlign: "center",
+			}}
+		>
 			<h1>요청하신 페이지를 찾을 수 없습니다.</h1>
-			<Link to="/">홈으로 돌아가기</Link>
+			<Link
+				style={{ textDecoration: "none", color: "#0087e8", fontWeight: "700", marginTop: "1rem" }}
+				to="/"
+			>
+				홈으로 돌아가기
+			</Link>
 		</div>
 	);
 }
