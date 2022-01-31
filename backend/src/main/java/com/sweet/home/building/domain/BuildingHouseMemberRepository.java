837750bd3818d08b3830e72947f92d399d5318e8
IcedCaffeLatte <tmdrl0160@naver.com>
2022-01-31 14:33:15 +0900

    [#75] feat: BuildingHouseMember 검색 관련 매서드 및 클래스 추가

--- backend/src/main/java/com/sweet/home/building/domain/BuildingHouseMemberRepository.java
+++ backend/src/main/java/com/sweet/home/building/domain/BuildingHouseMemberRepository.java
@@ -1,7 +1,13 @@
 package com.sweet.home.building.domain;
 
+import com.sweet.home.member.domain.Member;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.EntityGraph.EntityGraphType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BuildingHouseMemberRepository extends JpaRepository<BuildingHouseMember, Long> {
 
+    @EntityGraph(attributePaths = {"buildingHouse"}, type = EntityGraphType.FETCH)
+    Optional<BuildingHouseMember> findByMember(Member member);
 }
