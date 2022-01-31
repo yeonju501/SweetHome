5c88be7bae3fc114a1ab837114bdf11a2a64e40b
yeonju501 <loling970124@gmail.com>
2022-01-31 23:33:33 +0900

    [#94] feat: findByMemberAndReply 추가

--- backend/src/main/java/com/sweet/home/reply/domain/ReplyLikeRepository.java
+++ backend/src/main/java/com/sweet/home/reply/domain/ReplyLikeRepository.java
@@ -1,9 +1,12 @@
 package com.sweet.home.reply.domain;
 
 import com.sweet.home.member.domain.Member;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReplyLikeRepository extends JpaRepository<ReplyLike, Long> {
 
     boolean existsByMemberAndReply(Member member, Reply reply);
+
+    Optional<ReplyLike> findByMemberAndReply(Member member, Reply reply);
 }
