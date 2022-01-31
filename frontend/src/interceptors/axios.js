e4e144f0108c33701aa69eb6facf36eba068efbc
우동진 <wjinh93@gmail.com>
2022-02-01 01:29:40 +0900

    [#86] feat: refresh토큰으로 새 accesToken 요청 함수 구현

--- frontend/src/interceptors/axios.js
+++ frontend/src/interceptors/axios.js
@@ -1,1 +1,29 @@
 import axios from "axios";
+import { useSelector } from "react-redux";
+
+const URL = process.env.REACT_APP_SERVER_URL;
+
+axios.defaults.baseURL = `${URL}/api/`;
+
+axios.interceptors.response.use(
+	(res) => res,
+	async (error) => {
+		if (error.response.status === 401) {
+			const response = await axios({
+				url: "members/reissue",
+				headers: {
+					"Content-Type": "application/json;charset=UTF-8",
+					access_token,
+					refresh_token,
+				},
+			});
+
+			if (response.status === 200) {
+				axios.defaults.headers.common["Authorization"] = `Bearer ${response.data.access_token}`;
+
+				return axios(error.config);
+			}
+		}
+		return error;
+	},
+);
