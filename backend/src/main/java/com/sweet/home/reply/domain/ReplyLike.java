78a7a15aeb4f11d83e56a09e4f39f774269078c6
yeonju501 <loling970124@gmail.com>
2022-01-31 23:09:08 +0900

    [#93] feat: ReplyLike Entity, Repository 추가

---
+++ backend/src/main/java/com/sweet/home/reply/domain/ReplyLike.java
@@ -0,0 +1,46 @@
+package com.sweet.home.reply.domain;
+
+import com.sweet.home.member.domain.Member;
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+
+@Entity
+@Getter
+public class ReplyLike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "reply_like_id")
+    private Long id;
+
+    @ManyToOne(targetEntity = Reply.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "reply_id")
+    private Reply reply;
+
+    @ManyToOne(targetEntity = Member.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    protected ReplyLike() {
+    }
+
+    @Builder
+    public ReplyLike(Reply reply, Member member) {
+        this.reply = reply;
+        this.member = member;
+    }
+}
