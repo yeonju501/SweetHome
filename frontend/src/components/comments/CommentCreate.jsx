c17f2d200b615f4fe8d5cb57d3b4c48517d2965a
우동진 <wjinh93@gmail.com>
2022-02-01 20:12:46 +0900

    [#98] feat: onSubmit 함수 생성

--- frontend/src/components/comments/CommentCreate.jsx
+++ frontend/src/components/comments/CommentCreate.jsx
@@ -1,15 +1,28 @@
 import { useState } from "react";
+import axios from "axios";
 
 function CommentCreate() {
+	const URL = process.env.REACT_APP_SERVER_URL;
 	const [comment, setComment] = useState("");
 
 	const onChange = (e) => {
 		setComment(e.target.value);
 	};
 
+	const onSubmit = (e) => {
+		e.preventDefault();
+		axios({
+			url: `${URL}//api/articles/${articleId}/comments`,
+			method: "post",
+			data: comment,
+		});
+	};
+
 	return (
 		<div>
-			<input type="text" onChange={onChange} value={comment} placeholder="댓글을 남겨 보세요" />
+			<form onSubmit={onSubmit}>
+				<input type="text" onChange={onChange} value={comment} placeholder="댓글을 남겨 보세요" />
+			</form>
 		</div>
 	);
 }
