ef1f1ef6bd74294771535d60ed1a6cd9d3f68868
IcedCaffeLatte <tmdrl0160@naver.com>
2022-02-02 11:19:45 +0900

    [#88] feat: AgreedHouseResponse 추가

---
+++ backend/src/main/java/com/sweet/home/agreement/controller/dto/response/AgreedHouseResponse.java
@@ -0,0 +1,41 @@
+package com.sweet.home.agreement.controller.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.sweet.home.agreement.domain.AgreedHouse;
+import java.time.LocalDateTime;
+
+public class AgreedHouseResponse {
+
+    @JsonProperty("dong")
+    private String dong;
+
+    @JsonProperty("ho")
+    private String ho;
+
+    @JsonProperty("agreement_status")
+    private Boolean agreementStatus;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    @JsonProperty("created_at")
+    private LocalDateTime createdAt;
+
+    public AgreedHouseResponse() {
+    }
+
+    public AgreedHouseResponse(String dong, String ho, Boolean agreementStatus, LocalDateTime createdAt) {
+        this.dong = dong;
+        this.ho = ho;
+        this.agreementStatus = agreementStatus;
+        this.createdAt = createdAt;
+    }
+
+    public static AgreedHouseResponse from(AgreedHouse agreedHouse) {
+        return new AgreedHouseResponse(
+            agreedHouse.getBuildingHouse().getDong(),
+            agreedHouse.getBuildingHouse().getHo(),
+            agreedHouse.isAgreement_status(),
+            agreedHouse.getCreatedAt()
+        );
+    }
+}
