fe235addbd7d6d7a4b4f79f7863b3f05833eb652
IcedCaffeLatte <tmdrl0160@naver.com>
2022-02-02 11:19:55 +0900

    [#88] feat: AgreedHouseRepository 추가

--- backend/src/main/java/com/sweet/home/agreement/domain/AgreedHouseRepository.java
+++ backend/src/main/java/com/sweet/home/agreement/domain/AgreedHouseRepository.java
@@ -1,7 +1,9 @@
 package com.sweet.home.agreement.domain;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AgreedHouseRepository extends JpaRepository<AgreedHouse, Long> {
 
+    List<AgreedHouse> findByAgreement(Agreement agreement);
 }
