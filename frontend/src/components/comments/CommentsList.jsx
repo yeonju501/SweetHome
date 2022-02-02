df7ce7e1a6a5455619ca0fb50a65142f43fdbc7b
우동진 <wjinh93@gmail.com>
2022-02-02 13:31:50 +0900

    [#101] refactor: 조건문 삭제

--- frontend/src/components/comments/CommentsList.jsx
+++ frontend/src/components/comments/CommentsList.jsx
@@ -3,14 +3,13 @@ import React from "react";
 function CommentsList({ comments }) {
 	return (
 		<ul>
-			{comments &&
-				comments.map((comment) => (
-					<li>
-						<p>{comment.username}</p>
-						<p>{comment.content}</p>
-						<p>{comment.created_at}</p>
-					</li>
-				))}
+			{comments.map((comment) => (
+				<li>
+					<p>{comment.username}</p>
+					<p>{comment.content}</p>
+					<p>{comment.created_at}</p>
+				</li>
+			))}
 		</ul>
 	);
 }
