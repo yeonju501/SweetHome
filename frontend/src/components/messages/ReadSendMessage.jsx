88a0f5f952f3fd918a367ce355afe3ccd395afa6
vtaco <woosj7129@naver.com>
2022-02-03 17:54:17 +0900

    [#110] style: ReadSendMessage.jsx pageSize useState() 선언

--- frontend/src/components/messages/ReadSendMessage.jsx
+++ frontend/src/components/messages/ReadSendMessage.jsx
@@ -11,6 +11,7 @@ function ReadSendMessage() {
 	const [page, setPage] = useState(0);
 	const size = 10;
 	const [checkItems, setCheckITems] = useState([]);
+	const [pageSize, setPageSize] = useState(0);
 
 	useEffect(() => {
 		axios({
@@ -20,6 +21,7 @@ function ReadSendMessage() {
 		})
 			.then((res) => {
 				setSendMessageArray(res.data.messages);
+				setPageSize(res.data.total_page_count);
 				console.log(res.data);
 				console.log("저장된 값 확인", sendMessageArray);
 			})
@@ -79,8 +81,10 @@ function ReadSendMessage() {
 					</li>
 				))}
 			</ul>
-			<button onClick={pageDown}>이전</button>
-			<button onClick={pageUp}>다음</button>
+			<div>
+				<button onClick={pageDown}>이전</button>
+				<button onClick={pageUp}>다음</button>
+			</div>
 		</div>
 	);
 }
