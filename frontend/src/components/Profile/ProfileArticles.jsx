8e47b39fd947cd3819178e066224a3f50bc67e93
우동진 <wjinh93@gmail.com>
2022-02-04 00:37:53 +0900

    [#131] feat: axios 및 useState생성

--- frontend/src/components/Profile/ProfileArticles.jsx
+++ frontend/src/components/Profile/ProfileArticles.jsx
@@ -1,5 +1,15 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 function ProfileArticles() {
-	return <div></div>;
+	const URL = process.env.REACT_APP_SERVER_URL;
+	const [articles, setArticles] = useState("");
+
+	useEffect(() => {
+		axios.get(`${URL}/api/articles/mine`).then((res) => setArticles(res.data));
+	});
+
+	return <div>{articles && articles.map((article) => <p>article</p>)}</div>;
 }
 
 export default ProfileArticles;
