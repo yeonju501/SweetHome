1ca328004f1c54d7544f7d3fa492c8d414c1c6b3
9603100 <87457698+9603100@users.noreply.github.com>
2022-02-03 11:45:40 +0900

    [#106] feat: 게시글 수정 컴포넌트 구현

--- frontend/src/components/articles/ArticleDetail.jsx
+++ frontend/src/components/articles/ArticleDetail.jsx
@@ -2,13 +2,13 @@ import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import axios from "axios";
 import Comments from "../comments/Comments";
+import ArticleUpdate from "./ArticleUpdate";
 
 const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 function ArticleDetail({ articleId, currentBoard, setArticleClicked }) {
-	const token = useSelector((state) => state.token.token);
-
 	const [articleData, setArticleData] = useState();
+	const [update, setUpdate] = useState(false);
 
 	useEffect(() => {
 		axios({
@@ -17,9 +17,9 @@ function ArticleDetail({ articleId, currentBoard, setArticleClicked }) {
 		}).then((res) => {
 			setArticleData(res.data);
 		});
-	}, []);
+	}, [update]);
 
-	const handleDeleteButton = () => {
+	const handleDeleteButtonClick = () => {
 		axios({
 			url: `${SERVER_URL}/api/boards/articles/${articleId}`,
 			method: "delete",
@@ -28,35 +28,43 @@ function ArticleDetail({ articleId, currentBoard, setArticleClicked }) {
 		});
 	};
 
+	const handleUpdateButtonClick = () => {
+		setUpdate(true);
+	};
+
 	return (
 		<div>
-			{articleData && (
-				<div>
-					<div>{currentBoard.name}</div>
-					<article>
-						<div>
-							<p>{articleData.username}</p>
-							<p>{articleData.created_at}</p>
-						</div>
-						<div>
-							<button>쪽지</button>
-							<button>신고</button>
-						</div>
-						<div>
-							<button>수정</button>
-							<button onClick={handleDeleteButton}>삭제</button>
-						</div>
-						<h3>{articleData.title}</h3>
-						<p>{articleData.content}</p>
-						<div>
-							<span>{articleData.total_likes}</span>
-							<span>댓글 수</span>
-							<button>🤍</button>
-						</div>
-					</article>
-					<Comments articleId={articleId} />
-				</div>
-			)}
+			<nav>nav-bar</nav>
+			{articleData &&
+				(update ? (
+					<ArticleUpdate articleId={articleId} setUpdate={setUpdate} />
+				) : (
+					<div>
+						<div>{currentBoard.name}</div>
+						<article>
+							<div>
+								<p>{articleData.username}</p>
+								<p>{articleData.created_at}</p>
+							</div>
+							<div>
+								<button>쪽지</button>
+								<button>신고</button>
+							</div>
+							<div>
+								<button onClick={handleUpdateButtonClick}>수정</button>
+								<button onClick={handleDeleteButtonClick}>삭제</button>
+							</div>
+							<h3>{articleData.title}</h3>
+							<p>{articleData.content}</p>
+							<div>
+								<span>{articleData.total_likes}</span>
+								<span>댓글 수</span>
+								<button>🤍</button>
+							</div>
+						</article>
+						<Comments articleId={articleId} />
+					</div>
+				))}
 		</div>
 	);
 }
