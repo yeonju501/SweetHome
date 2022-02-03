6e4451944ab0aa20bf2011fce461288cdff2fa96
yeonju501 <loling970124@gmail.com>
2022-02-03 13:49:06 +0900

    [#116] feat: Comment Reply 엔티티 병합

--- backend/src/main/java/com/sweet/home/comment/domain/Comment.java
+++ backend/src/main/java/com/sweet/home/comment/domain/Comment.java
@@ -4,8 +4,8 @@ import com.sweet.home.article.domain.Article;
 import com.sweet.home.global.exception.BusinessException;
 import com.sweet.home.global.exception.ErrorCode;
 import com.sweet.home.member.domain.Member;
-import com.sweet.home.reply.domain.Reply;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import javax.persistence.Basic;
@@ -18,22 +18,25 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.Transient;
 import lombok.Builder;
 import lombok.Getter;
 import org.hibernate.annotations.Formula;
 import org.hibernate.annotations.Where;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 
 @Getter
 @Entity
-@Where(clause = "deleted_at is null and blocked_at is null")
+@Where(clause = "blocked_at is null")
 public class Comment {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "comment_id")
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Comment parent;
 
     @ManyToOne(targetEntity = Article.class, fetch = FetchType.LAZY)
     @JoinColumn(name = "article_id")
@@ -46,19 +49,8 @@ public class Comment {
     @Column(name = "content", nullable = false)
     private String content;
 
-    @CreatedDate
-    @Column(name = "created_at")
-    private LocalDateTime createdAt;
-
-    @LastModifiedDate
-    @Column(name = "updated_at", nullable = true)
-    private LocalDateTime updatedAt;
-
     @Column(name = "blocked_at", nullable = true)
     private LocalDateTime blockedAt;
-
-    @Column(name = "deleted_at", nullable = true)
-    private LocalDateTime deletedAt;
 
     @Basic(fetch = FetchType.LAZY)
     @Formula("(select count(1) from comment_like cl where cl.comment_id = comment_id)")
@@ -68,16 +60,18 @@ public class Comment {
     @Formula("(select count(1) from comment_report cr where cr.comment_id = comment_id)")
     private long totalReports;
 
-    @OneToMany(mappedBy = "comment", targetEntity = Reply.class, fetch = FetchType.LAZY)
-    private List<Reply> replies;
+    @OneToMany(mappedBy = "parent")
+    private List<Comment> childList = new ArrayList<>();
 
+    @Transient
     private static final int BLOCK_STANDARD = 5;
 
     protected Comment() {
     }
 
     @Builder
-    public Comment(Article article, Member member, String content) {
+    public Comment(Comment parent, Article article, Member member, String content) {
+        this.parent = parent;
         this.article = article;
         this.member = member;
         this.content = content;
@@ -93,10 +87,6 @@ public class Comment {
         if (!Objects.isNull(content)) {
             this.content = content;
         }
-    }
-
-    public void deleteComment() {
-        this.deletedAt = LocalDateTime.now();
     }
 
     public void checkTotalReports() {
