b881f522780fbb71abe6edced8f2ac18455c3c17
yeonju501 <loling970124@gmail.com>
2022-02-05 02:26:30 +0900

    [#153] feat: findAllByMember @EntityGraph 설정

--- backend/src/main/java/com/sweet/home/article/domain/ArticleLikeRepository.java
+++ backend/src/main/java/com/sweet/home/article/domain/ArticleLikeRepository.java
@@ -4,6 +4,8 @@ import com.sweet.home.member.domain.Member;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.EntityGraph.EntityGraphType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ArticleLikeRepository extends JpaRepository<ArticleLike, Long> {
@@ -12,5 +14,6 @@ public interface ArticleLikeRepository extends JpaRepository<ArticleLike, Long> 
 
     Optional<ArticleLike> findByMemberAndArticle(Member member, Article article);
 
+    @EntityGraph(attributePaths = {"member"}, type = EntityGraphType.FETCH)
     Page<ArticleLike> findAllByMember(Member member, Pageable pageable);
 }
