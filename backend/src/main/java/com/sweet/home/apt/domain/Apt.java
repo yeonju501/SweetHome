ed1d77367d713e2370f24afd0ac38bb55aed02f9
IcedCaffeLatte <tmdrl0160@naver.com>
2022-02-04 15:10:02 +0900

    [#142] feat: Apt 에 Entity 추가

--- backend/src/main/java/com/sweet/home/apt/domain/Apt.java
+++ backend/src/main/java/com/sweet/home/apt/domain/Apt.java
@@ -17,6 +17,24 @@ public class Apt {
     @Column(name = "apt_id")
     private Long id;
 
+    @Column(name = "adm_code")
+    private int admCode;
+
+    @Column(name = "sido")
+    private String sido;
+
+    @Column(name = "road_name")
+    private String roadName;
+
+    @Column(name = "road_code")
+    private int roadCode;
+
+    @Column(name = "apt_main_num")
+    private int aptMainNum;
+
+    @Column(name = "apt_sub_num")
+    private int aptSubNum;
+
     @Column(name = "name")
     private String name;
 
@@ -24,7 +42,14 @@ public class Apt {
     }
 
     @Builder
-    public Apt(String name) {
+    public Apt(Long id, int admCode, String sido, String roadName, int roadCode, int aptMainNum, int aptSubNum, String name) {
+        this.id = id;
+        this.admCode = admCode;
+        this.sido = sido;
+        this.roadName = roadName;
+        this.roadCode = roadCode;
+        this.aptMainNum = aptMainNum;
+        this.aptSubNum = aptSubNum;
         this.name = name;
     }
 }
