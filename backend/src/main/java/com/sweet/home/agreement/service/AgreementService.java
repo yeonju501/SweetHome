01bf910b5502127b3e2f73538ccfd24184ed0e85
IcedCaffeLatte <tmdrl0160@naver.com>
2022-02-05 13:50:52 +0900

    [#143] refactor: 임시코드 모두 제거 및 현재 테이블에 맞게 수정

--- backend/src/main/java/com/sweet/home/agreement/service/AgreementService.java
+++ backend/src/main/java/com/sweet/home/agreement/service/AgreementService.java
@@ -5,12 +5,12 @@ import com.sweet.home.agreement.controller.dto.request.AgreementRequest;
 import com.sweet.home.agreement.controller.dto.response.AgreedHouseResponse;
 import com.sweet.home.agreement.controller.dto.response.AgreementDetailResponse;
 import com.sweet.home.agreement.controller.dto.response.AgreementResponse;
+import com.sweet.home.agreement.domain.AgreedHouse;
 import com.sweet.home.agreement.domain.AgreedHouseRepository;
 import com.sweet.home.agreement.domain.Agreement;
 import com.sweet.home.agreement.domain.AgreementRepository;
 import com.sweet.home.apt.domain.Apt;
 import com.sweet.home.apt.domain.AptHouse;
-import com.sweet.home.apt.domain.AptRepository;
 import com.sweet.home.apt.service.AptService;
 import com.sweet.home.global.exception.BusinessException;
 import com.sweet.home.global.exception.ErrorCode;
@@ -54,14 +54,7 @@ public class AgreementService {
         Agreement agreement = agreementRepository.findById(agreementId)
             .orElseThrow(() -> new BusinessException(ErrorCode.AGREEMENT_NOT_FOUND_BY_ID));
 
-        //TODO: 멤버가 이 동의서의 아파트를 관리하는지 확인하는 로직
-        // 임시코드
-        // Member에서 찾은 AptMember에서의 apt_id 필요
-
-        Apt apt = aptService.findById(1L);
-        // 임시코드 끝
-
-        agreement.checkAptRelationship(apt);
+        agreement.checkAptRelationship(member.getAptHouse().getApt());
 
         agreement.saveDeletedTime();
     }
@@ -72,13 +65,7 @@ public class AgreementService {
         Agreement agreement = agreementRepository.findById(agreementId)
             .orElseThrow(() -> new BusinessException(ErrorCode.AGREEMENT_NOT_FOUND_BY_ID));
 
-        //TODO: 멤버가 이 동의서의 아파트를 관리하는지 확인하는 로직
-        // 임시코드
-        // member에서 찾은 aptMember에서의 apt_id 필요
-        Apt apt = aptService.findById(1L);
-        // 임시코드 끝
-
-        agreement.checkAptRelationship(apt);
+        agreement.checkAptRelationship(member.getAptHouse().getApt());
 
         agreement.changeTitle(request.getTitle());
         agreement.changeContent(request.getContent());
@@ -92,13 +79,7 @@ public class AgreementService {
         Agreement agreement = agreementRepository.findById(agreementId)
             .orElseThrow(() -> new BusinessException(ErrorCode.AGREEMENT_NOT_FOUND_BY_ID));
 
-        //TODO: 멤버가 이 동의서의 아파트에서 살고있는지 확인하는 로직
-        // 임시코드
-        // member에서 찾은 aptHouseMember에서 찾은 aptHouse 의 apt_id 필요
-        Apt apt = aptService.findById(1L);
-        // 임시코드 끝
-
-        agreement.checkAptRelationship(apt);
+        agreement.checkAptRelationship(member.getAptHouse().getApt());
 
         return AgreementDetailResponse.from(agreement);
     }
@@ -107,39 +88,21 @@ public class AgreementService {
     public List<AgreementResponse> viewAgreements(Pageable pageable, String email) {
         Member member = memberService.findByEmail(email);
 
-        //TODO: 멤버가 이 동의서의 아파트에서 살고있는지 확인하는 로직
-        // 임시코드
-        // member에서 찾은 aptHouseMember에서 찾은 aptHouse 의 apt_id 필요
-        Apt apt = aptService.findById(1L);
-        // 임시코드 끝
-
-        return agreementRepository.findByApt(apt, pageable).stream()
+        return agreementRepository.findByApt(member.getAptHouse().getApt(), pageable).stream()
             .map(AgreementResponse::from)
             .collect(Collectors.toList());
     }
 
     @Transactional
     public void createAgree(String email, Long agreementId, AgreeRequest request) {
-        //멤버 찾기
         Member member = memberService.findByEmail(email);
-        //멤버의 aptHouseMember 찾기
-        //AptHouseMember aptHouseMember = aptHouseMemberService.findByMember(member);
-        //동의서 ID 찾기
         Agreement agreement = agreementRepository.findById(agreementId)
             .orElseThrow(() -> new BusinessException(ErrorCode.AGREEMENT_NOT_FOUND_BY_ID));
 
-        //동의서의 aptId와 멤버에서 찾은 aptHouse의 AptID 가 같은지 확인
-        //checkSameApt(agreement.getApt(), aptHouseMember.getAptHouse().getApt());
-        //이미 서명한 세대인지 확인
-        //checkDuplicateHouse(aptHouseMember.getAptHouse());
+        member.checkAptMember(agreement.getApt());
+        checkDuplicateHouse(member.getAptHouse());
 
-        //agreedHouseRepository.save(AgreedHouse.createAgree(agreement, aptHouseMember.getAptHouse(), request));
-    }
-
-    private void checkSameApt(Apt firstApt, Apt secondApt) {
-        if (!firstApt.equals(secondApt)) {
-            throw new BusinessException(ErrorCode.APT_NOT_MATCH_BY_APT_ID);
-        }
+        agreedHouseRepository.save(AgreedHouse.createAgree(agreement, member.getAptHouse(), request));
     }
 
     private void checkDuplicateHouse(AptHouse aptHouse) {
@@ -151,11 +114,10 @@ public class AgreementService {
     @Transactional(readOnly = true)
     public List<AgreedHouseResponse> viewAgreedHouses(String email, Long agreementId) {
         Member member = memberService.findByEmail(email);
-
-        //TODO: 이 멤버가 해당 동의서의 아파트 관리자인지 찾기
-
         Agreement agreement = agreementRepository.findById(agreementId)
             .orElseThrow(() -> new BusinessException(ErrorCode.AGREEMENT_NOT_FOUND_BY_ID));
+
+        agreement.checkAptRelationship(member.getAptHouse().getApt());
 
         return agreedHouseRepository.findByAgreement(agreement).stream()
             .map(AgreedHouseResponse::from)
