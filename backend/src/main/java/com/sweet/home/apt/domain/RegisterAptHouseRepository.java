8110e24b8031def19b50bc879e61cbc182c58d34
IcedCaffeLatte <tmdrl0160@naver.com>
2022-02-06 15:21:38 +0900

    [#168] feat: RegisterAptHouseRepository findByMember 추가

--- backend/src/main/java/com/sweet/home/apt/domain/RegisterAptHouseRepository.java
+++ backend/src/main/java/com/sweet/home/apt/domain/RegisterAptHouseRepository.java
@@ -1,8 +1,11 @@
 package com.sweet.home.apt.domain;
 
 import com.sweet.home.member.domain.Member;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RegisterAptHouseRepository extends JpaRepository<RegisterAptHouse, Long> {
     boolean existsByMember(Member member);
+
+    Optional<RegisterAptHouse> findByMember (Member member);
 }
