41ec1d2a4b71f0e446d866cf04f710ac1c827d31
IcedCaffeLatte <tmdrl0160@naver.com>
2022-02-06 19:37:05 +0900

    [#170] feat: ErrorResponse 에 toJson 추가

--- backend/src/main/java/com/sweet/home/global/exception/ErrorResponse.java
+++ backend/src/main/java/com/sweet/home/global/exception/ErrorResponse.java
@@ -1,8 +1,11 @@
 package com.sweet.home.global.exception;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ErrorResponse {
+
     @JsonProperty("error_code")
     private String errorCode;
 
@@ -13,7 +16,12 @@ public class ErrorResponse {
         this.errorCode = errorCode;
     }
 
-    public static ErrorResponse from(ErrorCode errorCode){
+    public static ErrorResponse from(ErrorCode errorCode) {
         return new ErrorResponse(errorCode.getCode());
+    }
+
+    public static String toJson(JwtException e) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(new ErrorResponse(e.getErrorCode().getCode()));
     }
 }
