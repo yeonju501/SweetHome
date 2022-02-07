40370405e29aa30b1b5699d11bc17cc105087028
yeonju501 <loling970124@gmail.com>
2022-02-07 13:27:02 +0900

    [#160] feat: oauth 설정 추가

--- backend/src/main/java/com/sweet/home/global/config/WebSecurityConfig.java
+++ backend/src/main/java/com/sweet/home/global/config/WebSecurityConfig.java
@@ -1,10 +1,13 @@
 package com.sweet.home.global.config;
 
+import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpMethod.POST;
 
 import com.sweet.home.auth.infrastructure.JwtTokenProvider;
+import com.sweet.home.auth.service.CustomOAuth2UserService;
 import com.sweet.home.global.security.JwtAccessDeniedHandler;
 import com.sweet.home.global.security.JwtAuthenticationEntryPoint;
+import com.sweet.home.global.security.OAuth2SuccessHandler;
 import java.util.Arrays;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,16 +25,20 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final JwtTokenProvider jwtTokenProvider;
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final OAuth2SuccessHandler oAuth2SuccessHandler;
 
-    public WebSecurityConfig(JwtTokenProvider jwtTokenProvider,
-        JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
-        JwtAccessDeniedHandler jwtAccessDeniedHandler) {
-        this.jwtTokenProvider = jwtTokenProvider;
+    public WebSecurityConfig(JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+        JwtAccessDeniedHandler jwtAccessDeniedHandler, CustomOAuth2UserService customOAuth2UserService,
+        JwtTokenProvider jwtTokenProvider, OAuth2SuccessHandler oAuth2SuccessHandler) {
         this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
         this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+        this.customOAuth2UserService = customOAuth2UserService;
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.oAuth2SuccessHandler = oAuth2SuccessHandler;
     }
 
     @Bean
@@ -74,15 +81,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .and()
 
             .authorizeRequests()
+            .antMatchers(GET, "/oauth2/**").permitAll()
             .antMatchers(POST, "/api/members/join").permitAll()
             .antMatchers(POST, "/api/members/reissue").permitAll()
             .antMatchers(POST, "/api/members/login").permitAll()
             .anyRequest().authenticated()
             .and()
-
             .formLogin()
             .disable()
             .apply(new JwtSecurityConfig(jwtTokenProvider))
+            .and()
+
+            .oauth2Login()
+            .userInfoEndpoint()
+            .userService(customOAuth2UserService)
+            .and()
+            .successHandler(oAuth2SuccessHandler)
         ;
     }
 }
