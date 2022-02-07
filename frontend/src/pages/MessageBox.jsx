daa234e850521590c25759cb788673aae9285a56
vtaco <woosj7129@naver.com>
2022-02-07 16:12:33 +0900

    [#172] style: Message.jsx 불필요한 import 삭제

--- frontend/src/pages/MessageBox.jsx
+++ frontend/src/pages/MessageBox.jsx
@@ -1,8 +1,4 @@
-import React, { useState } from "react";
-import { Link, Outlet } from "react-router-dom";
-import style from "../style/Messages.module.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { SET_POSITION } from "../store/toggle";
