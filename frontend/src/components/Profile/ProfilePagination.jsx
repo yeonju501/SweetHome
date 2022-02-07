7d710e78b4883fc8c33fbd31eac7a542aa61118f
우동진 <wjinh93@gmail.com>
2022-02-07 09:52:23 +0900

    [#171] feat: Pagination 컴포넌트 구현

---
+++ frontend/src/components/Profile/ProfilePagination.jsx
@@ -0,0 +1,36 @@
+function ProfilePagination() {
+	return (
+		<>
+			<nav className={style.pagination}>
+				<button
+					className={style.btn_pagination}
+					onClick={() => setData((prev) => ({ ...prev, currentPage: page - 1 }))}
+					disabled={page === 0}
+				>
+					&lt;
+				</button>
+				{Array(total)
+					.fill()
+					.map((_, i) => (
+						<button
+							className={style.btn_pagination}
+							key={i + 1}
+							onClick={() => setData((prev) => ({ ...prev, currentPage: i }))}
+							aria-current={page === i ? "page" : null}
+						>
+							{i + 1}
+						</button>
+					))}
+				<button
+					className={style.btn_pagination}
+					onClick={() => setData((prev) => ({ ...prev, currentPage: page + 1 }))}
+					disabled={page === total - 1}
+				>
+					&gt;
+				</button>
+			</nav>
+		</>
+	);
+}
+
+export default ProfilePagination;
