7c847ad8202fd2414459f26dd4d43d535a3fc88e
vtaco <woosj7129@naver.com>
2022-02-07 15:52:54 +0900

    [#172] style: index.js toggle 참조

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import token from "./token";
 import userInfo from "./user";
+import toggle from "./toggle";
 
 const rootReducer = combineReducers({
 	token,
 	userInfo,
+	toggle,
 });
 
 export default rootReducer;
