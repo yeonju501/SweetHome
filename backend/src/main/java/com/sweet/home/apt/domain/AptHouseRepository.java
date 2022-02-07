04f5eb4bf38e998cd3fb344626cc8a967dc57885
IcedCaffeLatte <tmdrl0160@naver.com>
2022-02-07 13:57:55 +0900

    [#175] feat: AptHouseRepository 에 findByAptAndDongAndHo 추가

--- backend/src/main/java/com/sweet/home/apt/domain/AptHouseRepository.java
+++ backend/src/main/java/com/sweet/home/apt/domain/AptHouseRepository.java
@@ -1,7 +1,9 @@
 package com.sweet.home.apt.domain;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AptHouseRepository extends JpaRepository<AptHouse, Long> {
 
+    Optional<AptHouse> findByAptAndDongAndHo(Apt apt, String dong, String ho);
 }
