217b8ae4e6ec2a76db067657e15e3eb016020d58
우동진 <wjinh93@gmail.com>
2022-02-08 00:45:50 +0900

    [#167] refactor: 폴더명 소문자로 변경

---
+++ frontend/src/components/profile/ProfileNav.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import style from "../../style/Profile.module.css";
+
+function ProfileNav({ active, setActive }) {
+	const arr = ["회원정보", "내가 작성한 글", "내가 작성한 댓글", "내가 좋아요한 글"];
+
+	return (
+		<div>
+			<ul className={style.nav}>
+				{arr.map((item, idx) => (
+					<li
+						className={active === idx ? style.active : style.deactive}
+						key={idx}
+						onClick={() => setActive(idx)}
+					>
+						{item}
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+}
+
+export default ProfileNav;
