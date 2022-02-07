e07e4396a9b2059ccc9588398f80fb872a15c32e
IcedCaffeLatte <tmdrl0160@naver.com>
2022-02-07 14:02:39 +0900

    [#175] feat: AptRestController 에 allowAptHouseMember 추가

--- backend/src/main/java/com/sweet/home/apt/controller/AptRestController.java
+++ backend/src/main/java/com/sweet/home/apt/controller/AptRestController.java
@@ -1,5 +1,6 @@
 package com.sweet.home.apt.controller;
 
+import com.sweet.home.apt.controller.dto.request.AllowAptHouseMemberRequest;
 import com.sweet.home.apt.controller.dto.request.RegisterAptHouseRequest;
 import com.sweet.home.apt.controller.dto.response.AptRegisterMembersResponse;
 import com.sweet.home.apt.controller.dto.response.MyRegisterAptHouseResponse;
@@ -50,18 +51,26 @@ public class AptRestController {
 
     //관리자영역
     //등록 요청 - 아파트 등록 요청 회원 목록 보기 = GET
-    @GetMapping("/apts/register")
+    @GetMapping("/admin/apts/register/")
     public ResponseEntity<AptRegisterMembersResponse> viewAptRegisterMembers(@AuthenticationPrincipal String email,
         Pageable pageable) {
         return ResponseEntity.ok().body(aptService.viewAptRegisterMembers(pageable, email));
     }
-    //등록 요청 - 아파트 등록 요청 승인하기 = POST
 
-    //등록 요청 - 아파트 등록 요청 다중 승인하기 = POST
+    //등록 요청 - 아파트 등록 요청 승인하기 = POST
+    @PostMapping("/admin/apts/register")
+    public ResponseEntity<Void> allowAptHouseMember(@AuthenticationPrincipal String email,
+        @RequestBody AllowAptHouseMemberRequest request) {
+        aptService.allowAptHouseMember(email, request);
+        URI uri = URI.create("/api/register/");
+        return ResponseEntity.created(uri).build();
+    }
+
+    //등록 요청 - 아파트 등록 요청 다중 승인하기 = POST / 나중에 할 것
 
     //등록 요청 - 아파트 등록 요청 거절하기 = ??
 
-    //등록 요청 - 아파트 등록 요청 다중 거절하기 = ??
+    //등록 요청 - 아파트 등록 요청 다중 거절하기 = ?? / 나중에 할것
 
     //아파트 회원 관리 - 관리하는 아파트의 회원 조회하기 = GET
 
