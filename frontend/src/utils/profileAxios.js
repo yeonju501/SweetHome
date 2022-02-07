e3b1cb47a3b5dc5c07ab243ece0285f45b7ae013
우동진 <wjinh93@gmail.com>
2022-02-07 10:39:39 +0900

    [#171] refactor: 함수 리팩토링

--- frontend/src/utils/profileAxios.js
+++ frontend/src/utils/profileAxios.js
@@ -15,7 +15,6 @@ export function GETMYCOMMENTS(func1, page) {
 		url: `${SERVER_URL}/api/articles/comments/mine/?page=${page}`,
 		method: "get",
 	}).then((res) => {
-		console.log(res.data);
 		func1((prev) => ({
 			...prev,
 			comments: res.data.comments,
@@ -25,29 +24,14 @@ export function GETMYCOMMENTS(func1, page) {
 	});
 }
 
-export function GETMYARTICLES(address, func1, page) {
+export function GETMYARTICLES(func1, address, page) {
 	axios({
 		url: `${SERVER_URL}/api/${address}/?page=${page}`,
 		method: "get",
 	}).then((res) => {
-		console.log(res.data);
 		func1((prev) => ({
 			...prev,
 			articles: res.data.articles,
-			totalPage: res.data.total_page_count,
-			currentPage: res.data.current_page_count,
-		}));
-	});
-}
-export function GETMYLIKES(address, func1, page) {
-	axios({
-		url: `${SERVER_URL}/api/${address}/?page=${page}`,
-		method: "get",
-	}).then((res) => {
-		console.log(res.data);
-		func1((prev) => ({
-			...prev,
-			likes: res.data.likes,
 			totalPage: res.data.total_page_count,
 			currentPage: res.data.current_page_count,
 		}));
