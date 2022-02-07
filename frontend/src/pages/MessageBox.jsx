fd6dd2017817078946ac3d081ba7c79ec8b5d4b7
vtaco <woosj7129@naver.com>
2022-02-07 16:11:59 +0900

    [#172] style: MessageBox.jsx 렌더링시 SET_POSITION에서 현재 위치 message로 세팅

--- frontend/src/pages/MessageBox.jsx
+++ frontend/src/pages/MessageBox.jsx
@@ -3,8 +3,16 @@ import { Link, Outlet } from "react-router-dom";
 import style from "../style/Messages.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { SET_POSITION } from "../store/toggle";
 
 function MessageBox() {
+	const dispatch = useDispatch();
+	const toggle = useSelector((state) => state.toggle.toggleValue);
+	useEffect(() => {
+		dispatch(SET_POSITION(toggle, "message"));
+	});
 	return <></>;
 }
 
