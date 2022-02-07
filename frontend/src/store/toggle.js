4f02ccb731162e1989f7a0901741519c4a9844c0
vtaco <woosj7129@naver.com>
2022-02-07 15:57:26 +0900

    [#172] style: toggle.js dispatch 함수 구현

--- frontend/src/store/toggle.js
+++ frontend/src/store/toggle.js
@@ -5,3 +5,15 @@ const toggleState = {
 	toggleValue: false,
 	position: "main",
 };
+
+export const SET_TOGGLE = (currentToggle, currentPositon) => ({
+	type: CLICK_TOGGLE,
+	currentToggle,
+	currentPositon,
+});
+
+export const SET_POSITION = (currentToggle, currentPositon) => ({
+	type: CHANGE_POSITION,
+	currentToggle,
+	currentPositon,
+});
