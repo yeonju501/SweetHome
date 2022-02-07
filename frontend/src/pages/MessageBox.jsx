08b2f05d1f371e6ecb38e21072ec3a8833921fbd
vtaco <woosj7129@naver.com>
2022-02-07 16:10:24 +0900

    [#172] style: MessageBox.jsx 불필요한 코드 삭제

--- frontend/src/pages/MessageBox.jsx
+++ frontend/src/pages/MessageBox.jsx
@@ -5,41 +5,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 function MessageBox() {
-	const [isOpen, setMenu] = useState(false);
-
-	const toggleMenu = () => {
-		setMenu(!isOpen);
-	};
-	return (
-		<div className={style.message_box_container}>
-			<div>
-				<FontAwesomeIcon onClick={toggleMenu} icon={faBars} />
-				{isOpen && (
-					<ul>
-						<li className={style.li_link}>
-							<Link className={style.link} to="read-receive-message">
-								받은 메시지
-							</Link>
-						</li>
-						<li className={style.li_link}>
-							<Link className={style.link} to="read-send-message">
-								보낸 메시지
-							</Link>
-						</li>
-						<li className={style.li_link}>
-							<Link className={style.link} to="send-message">
-								메시지 보내기
-							</Link>
-						</li>
-					</ul>
-				)}
-			</div>
-
-			<div>
-				<Outlet />
-			</div>
-		</div>
-	);
+	return <></>;
 }
 
 export default MessageBox;
