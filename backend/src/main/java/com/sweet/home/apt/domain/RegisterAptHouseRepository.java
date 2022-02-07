030e154357fec6aeb9ff1adb5b69a322b769e586
IcedCaffeLatte <tmdrl0160@naver.com>
2022-02-07 11:33:42 +0900

    [#174] feat: RegisterAptHouseRepository 에 findByApt 추가

--- backend/src/main/java/com/sweet/home/apt/domain/RegisterAptHouseRepository.java
+++ backend/src/main/java/com/sweet/home/apt/domain/RegisterAptHouseRepository.java
@@ -2,10 +2,18 @@ package com.sweet.home.apt.domain;
 
 import com.sweet.home.member.domain.Member;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.EntityGraph.EntityGraphType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RegisterAptHouseRepository extends JpaRepository<RegisterAptHouse, Long> {
+
     boolean existsByMember(Member member);
 
-    Optional<RegisterAptHouse> findByMember (Member member);
+    Optional<RegisterAptHouse> findByMember(Member member);
+
+    @EntityGraph(attributePaths = {"member"}, type = EntityGraphType.FETCH)
+    Page<RegisterAptHouse> findByApt(Apt apt, Pageable pageable);
 }
