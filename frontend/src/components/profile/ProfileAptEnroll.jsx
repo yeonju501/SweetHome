c827a378560599bc9e1db3b33a87908eed252472
vtaco <woosj7129@naver.com>
2022-02-09 00:35:26 +0900

    [#215] feature: ProfileAptEnroll.jsx style

--- frontend/src/components/profile/ProfileAptEnroll.jsx
+++ frontend/src/components/profile/ProfileAptEnroll.jsx
@@ -7,7 +7,22 @@ function ProfileAptEnroll() {
 		dong: "",
 		ho: "",
 	});
-	return <h1>아파트 등록 및 이사가기?</h1>;
+	return (
+		<>
+			<form>
+				<label htmlFor="apt_id">아파트 id</label>
+				<input type="text" id="apt_id" />
+				<label htmlFor="dong">동</label>
+				<input type="text" id="dong" />
+				<label htmlFor="ho">호</label>
+				<input type="text" id="ho" />
+
+				<div style={{ display: "flex", alignItems: "center" }}>
+					<button style={{ marginRight: "25rem" }}>변경</button>
+				</div>
+			</form>
+		</>
+	);
 }
 
 export default ProfileAptEnroll;
