880eb9fb61684f2f59927c1079f5598b3356acf2
우동진 <wjinh93@gmail.com>
2022-02-08 14:56:05 +0900

    [#195] refactor: axios interceptors 리팩토링

--- frontend/src/interceptors/tokenRequest.js
+++ frontend/src/interceptors/tokenRequest.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 import Cookies from "universal-cookie";
-
-const cookies = new Cookies();
+import { onReissueFail, tokenReissue } from "../utils/manageToken";
 
 axios.defaults.baseURL = `${URL}/api/`;
 let refresh = false;
+const cookies = new Cookies();
 
 axios.interceptors.response.use(
 	(res) => res,
@@ -12,44 +12,15 @@ axios.interceptors.response.use(
 		const { config } = err;
 		if (err.response.status === 403 && !refresh) {
 			const originalRequest = config;
-			const refresh_token = cookies.get("refreshToken");
-			const access_token = cookies.get("accessToken");
 			refresh = true;
-			const response = await axios({
-				url: "http://localhost:8080/api/members/reissue",
-				method: "post",
-				withCredentials: true,
-				headers: {
-					"Content-type": "application/json",
-					Authorization: "",
-				},
-				data: {
-					access_token,
-					refresh_token,
-				},
-			});
-			if (response.status === 200) {
-				const expires = 1000 * 3600 * 24 * 7;
-				axios.defaults.headers.common["Authorization"] = `Bearer ${response.data.access_token}`;
-				cookies.set("refreshToken", response.data.refresh_token, {
-					path: "/",
-					secure: true,
-					// httpOnly: true,
-					expires: new Date(Date.now() + expires),
-				});
-				cookies.set("accessToken", response.data.access_token, {
-					path: "/",
-					secure: true,
-					// httpOnly: true,
-					expires: new Date(Date.now() + 1000 * 60 * 30),
-				});
-
-				originalRequest.headers.Authorization = `Bearer ${response.data.access_token}`;
-				refresh = false;
-				return axios(originalRequest);
-			}
+			tokenReissue();
+			originalRequest.headers.Authorization = `Bearer ${cookies.get("accessToken")}`;
+			refresh = false;
+			return axios(originalRequest);
+		} else {
+			refresh = false;
+			onReissueFail();
+			return Promise.reject(err);
 		}
-		refresh = false;
-		return Promise.reject(err);
 	},
 );
