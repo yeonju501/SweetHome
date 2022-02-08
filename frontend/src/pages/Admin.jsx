7a70d8df6e6ba5136a0874531a1a1635fa1eb338
vtaco <woosj7129@naver.com>
2022-02-08 23:31:12 +0900

    [#206] style: dispatch에 필요한 인자 선언

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
 function Admin() {
+	const dispatch = useDispatch();
+	const toggle = useSelector((state) => state.toggle.toggleValue);
 	useEffect(() => {});
 	return <></>;
 }
