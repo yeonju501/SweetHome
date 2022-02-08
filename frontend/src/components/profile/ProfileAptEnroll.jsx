de77c7c047234aa96dec74825b040c8f6d3aa00b
vtaco <woosj7129@naver.com>
2022-02-09 00:45:49 +0900

    [#215] feature: ProfileAptEnroll.jsx 아파트 등록 onSubmit() 구현

--- frontend/src/components/profile/ProfileAptEnroll.jsx
+++ frontend/src/components/profile/ProfileAptEnroll.jsx
@@ -1,5 +1,8 @@
+import axios from "axios";
 import React from "react";
 import { useState } from "react";
+
+const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 function ProfileAptEnroll() {
 	const [aptEnroll, setAptEnroll] = useState({
@@ -8,12 +11,26 @@ function ProfileAptEnroll() {
 		ho: "",
 	});
 
+	const onSubmit = (e) => {
+		console.log("Enroll Submit");
+		e.preventDefault();
+		axios({
+			method: "POST",
+			url: `${SERVER_URL}/api/apts/register`,
+			data: aptEnroll,
+		})
+			.then((res) => {
+				console.log("성공");
+			})
+			.catch((err) => console.log(err));
+	};
+
 	const onChange = (e) => {
 		setAptEnroll({ ...aptEnroll, [e.target.id]: e.target.value });
 	};
 	return (
 		<>
-			<form>
+			<form onSubmit={onSubmit}>
 				<label htmlFor="apt_id">아파트 id</label>
 				<input type="text" id="apt_id" onChange={onChange} />
 				<label htmlFor="dong">동</label>
