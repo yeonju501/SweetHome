ffe82421ee69601e21aeb33bd6b3b64b044c22e4
9603100 <87457698+9603100@users.noreply.github.com>
2022-02-09 01:16:10 +0900

    [#203] feat: 동의서 생성 기능 구현

--- frontend/src/components/agreements/AgreementCreate.jsx
+++ frontend/src/components/agreements/AgreementCreate.jsx
@@ -1,13 +1,66 @@
+import { useState } from "react";
+import axios from "axios";
+
 function AgreementCreate() {
+	const SERVER_URL = process.env.REACT_APP_SERVER_URL;
+	const [agreementData, setAgreementData] = useState({
+		title: "",
+		content: "",
+		start_date: "",
+		end_date: "",
+	});
+	const { title, content, start_date, end_date } = agreementData;
+
+	const handleFormSubmit = (e) => {
+		e.preventDefault();
+
+		axios({
+			url: `${SERVER_URL}/api/admin/agreements`,
+			method: "post",
+			data: {
+				title,
+				content,
+				start_date: `${start_date}T00:00:00`,
+				end_date: `${end_date}T23:59:59`,
+			},
+		}).then((res) => {
+			setAgreementData({
+				title: "",
+				content: "",
+				start_date: "",
+				end_date: "",
+			});
+		});
+	};
+
+	const handleInputChange = (e) => {
+		setAgreementData({ ...agreementData, [e.target.id]: e.target.value });
+	};
+
 	return (
 		<div>
 			<h1>동의서 작성</h1>
 			<form>
-				<input type="text" placeholder="제목을 입력하세요"></input>
-				<input type="date"></input>
-				<textarea placeholder="동의서 내용을 입력하세요"></textarea>
-				<button>작성</button>
-				<button>취소</button>
+				<input
+					type="text"
+					id="title"
+					value={title}
+					onChange={handleInputChange}
+					placeholder="제목을 입력하세요"
+				/>
+				<label>시작 날짜</label>
+				<input type="date" id="start_date" value={start_date} onChange={handleInputChange} />
+				<label>종료 날짜</label>
+				<input type="date" id="end_date" value={end_date} onChange={handleInputChange} />
+
+				<textarea
+					id="content"
+					placeholder="동의서 내용을 입력하세요"
+					value={content}
+					onChange={handleInputChange}
+				></textarea>
+				<button onClick={handleFormSubmit}>작성</button>
+				<button type="button">취소</button>
 			</form>
 		</div>
 	);
