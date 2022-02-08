167938339976d62f9caa91e116e25a5345d7bf56
IcedCaffeLatte <tmdrl0160@naver.com>
2022-02-08 15:09:21 +0900

    [#190] feat: MemberRepository 에 Member의 AptHouse 의 Apt 가 같은 멤버를 찾는 쿼리문 추가

--- backend/src/main/java/com/sweet/home/member/domain/MemberRepository.java
+++ backend/src/main/java/com/sweet/home/member/domain/MemberRepository.java
@@ -1,7 +1,11 @@
 package com.sweet.home.member.domain;
 
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
@@ -10,4 +14,10 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
 
     Optional<Member> findByUsername(String username);
+
+    @Query (nativeQuery = true, value = "select * "
+        + "from member m "
+        + "join apt_house ah on m.apt_house_id = ah.apt_house_id "
+        + "where ah.apt_id = (:aptId)")
+    Page<Member> findByAptId(@Param("aptId") Long aptId, Pageable pageable);
 }
