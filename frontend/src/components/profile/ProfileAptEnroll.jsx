ca47204ae09944538a459068058f757398378529
vtaco <woosj7129@naver.com>
2022-02-09 00:36:46 +0900

    [#215] feature: ProfileAptEnroll.jsx onChange() 구현

--- frontend/src/components/profile/ProfileAptEnroll.jsx
+++ frontend/src/components/profile/ProfileAptEnroll.jsx
@@ -7,15 +7,19 @@ function ProfileAptEnroll() {
 		dong: "",
 		ho: "",
 	});
+
+	const onChange = (e) => {
+		setAptEnroll({ ...aptEnroll, [e.target.id]: e.target.value });
+	};
 	return (
 		<>
 			<form>
 				<label htmlFor="apt_id">아파트 id</label>
-				<input type="text" id="apt_id" />
+				<input type="text" id="apt_id" onChange={onChange} />
 				<label htmlFor="dong">동</label>
-				<input type="text" id="dong" />
+				<input type="text" id="dong" onChange={onChange} />
 				<label htmlFor="ho">호</label>
-				<input type="text" id="ho" />
+				<input type="text" id="ho" onChange={onChange} />
 
 				<div style={{ display: "flex", alignItems: "center" }}>
 					<button style={{ marginRight: "25rem" }}>변경</button>
