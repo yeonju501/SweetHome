9fc007e0c2cc5cbb572b2f4cb550534bd23bddfe
vtaco <woosj7129@naver.com>
2022-02-09 00:30:44 +0900

    [#215] feature: aptEnroll useState()

--- frontend/src/components/profile/ProfileAptEnroll.jsx
+++ frontend/src/components/profile/ProfileAptEnroll.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import { useState } from "react";
 
 function ProfileAptEnroll() {
+	const [aptEnroll, setAptEnroll] = useState({
+		apt_id: "",
+		dong: "",
+		ho: "",
+	});
 	return <h1>아파트 등록 및 이사가기?</h1>;
 }
 
