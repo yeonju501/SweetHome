94d2897647e39c3808395f232c3a566481c2d8a2
vtaco <woosj7129@naver.com>
2022-02-08 23:32:09 +0900

    [#206] style: SET_POSITION dispatch를 사용해서 사용

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
+import { SET_POSITION } from "../store/toggle";
 
 function Admin() {
 	const dispatch = useDispatch();
 	const toggle = useSelector((state) => state.toggle.toggleValue);
-	useEffect(() => {});
+	useEffect(() => {
+		dispatch(SET_POSITION(toggle, "admin"));
+	}, []);
 	return <></>;
 }
 
