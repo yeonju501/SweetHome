778828149c42721c6b7a3eb6c434d2e170415812
vtaco <woosj7129@naver.com>
2022-02-08 23:21:29 +0900

    [#197] feature: 보낸 메시지 카운트 출력

--- frontend/src/pages/MessageBox.jsx
+++ frontend/src/pages/MessageBox.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { SET_POSITION } from "../store/toggle";
-import { getReceiveMessageCount } from "../utils/messagesFunction";
+import { getReceiveMessageCount, getSendMessageCount } from "../utils/messagesFunction";
 
 function MessageBox() {
 	const dispatch = useDispatch();
@@ -12,14 +12,19 @@ function MessageBox() {
 		all_count: "",
 		unread_count: "",
 	});
+	const [sendMessageCnt, setSendMessageCnt] = useState({
+		all_count: "",
+	});
 	useEffect(() => {
 		dispatch(SET_POSITION(toggle, "message"));
 		getReceiveMessageCount(setReceiveMessageCnt);
-	});
+		getSendMessageCount(setSendMessageCnt);
+	}, []);
 	return (
 		<>
 			<p>받은 메시지 : {receiveMessageCnt.all_count}</p>
 			<p>안 읽은 메시지 : {receiveMessageCnt.unread_count}</p>
+			<p>보낸 메시지 : {sendMessageCnt.all_count}</p>
 		</>
 	);
 }
