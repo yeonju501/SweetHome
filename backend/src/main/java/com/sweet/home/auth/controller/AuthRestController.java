3229f134c08e5056fdc8cee67deda58fe8772922
yeonju501 <loling970124@gmail.com>
2022-02-10 03:02:52 +0900

    [#239] feat: kakao oauth 설정 변경

--- backend/src/main/java/com/sweet/home/auth/controller/AuthRestController.java
+++ backend/src/main/java/com/sweet/home/auth/controller/AuthRestController.java
@@ -4,10 +4,13 @@ import com.sweet.home.auth.controller.dto.request.LoginRequest;
 import com.sweet.home.auth.controller.dto.request.TokenRequest;
 import com.sweet.home.auth.controller.dto.response.TokenResponse;
 import com.sweet.home.auth.service.AuthService;
+import com.sweet.home.auth.service.KakaoOAuthService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,9 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthRestController {
 
     private final AuthService authService;
+    private final KakaoOAuthService kakaoOAuthService;
 
-    public AuthRestController(AuthService authService) {
+    public AuthRestController(AuthService authService, KakaoOAuthService kakaoOAuthService) {
         this.authService = authService;
+        this.kakaoOAuthService = kakaoOAuthService;
     }
 
     @PostMapping("/members/login")
@@ -28,5 +33,10 @@ public class AuthRestController {
     @PostMapping("/members/reissue")
     public ResponseEntity<TokenResponse> reissue(@RequestBody TokenRequest request) {
         return ResponseEntity.ok().body(authService.reissue(request));
+    }
+
+    @GetMapping("/oauth2/authorization/kakao")
+    public ResponseEntity<TokenResponse> getKakaoCode(@RequestParam String code){
+        return ResponseEntity.ok().body(kakaoOAuthService.oauth2AuthorizationKakao(code));
     }
 }
