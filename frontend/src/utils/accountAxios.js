5a37d479a5fe20d6caa67aae6f642f13fc34ea6c
우동진 <wjinh93@gmail.com>
2022-02-09 14:15:03 +0900

    [#231] refactor: submitAxios 함수 생성

---
+++ frontend/src/utils/accountAxios.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+import errorMessage from "../store/errorMessage";
+import { onLoginSuccess } from "./manageToken";
+export function submitAxios(api, inputValue, moveUrl, login?) {
+	const SERVER_URL = process.env.REACT_APP_SERVER_URL;
+	axios({
+		method: "post",
+		url: `${SERVER_URL}/api/members/${api}`,
+		withCredentials: true,
+		headers: {
+			"Content-type": "application/json",
+		},
+		data: inputValue,
+	})
+		.then((res) => {
+			login && onLoginSuccess(res);
+			window.location.replace(`${moveUrl}`);
+		})
+		.catch((err) => errorMessage(err.response.data.error_code));
+}
