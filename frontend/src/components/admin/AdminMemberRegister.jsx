ee557873c878984932465d1458cd74c8b559d01f
vtaco <woosj7129@naver.com>
2022-02-09 23:02:28 +0900

    [#216] feature: useState() 선언

--- frontend/src/components/admin/AdminMemberRegister.jsx
+++ frontend/src/components/admin/AdminMemberRegister.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { useState } from "react";
 import { useEffect } from "react";
 
 const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 function AdminMemberRegister() {
+	const [aptMemberRegister, setAptMemberRegister] = useState({
+		name: "",
+		email: "",
+		dong: "",
+		ho: "",
+		message: "",
+		phone_number: "",
+	});
 	useEffect(() => {}, []);
 	return;
 }
