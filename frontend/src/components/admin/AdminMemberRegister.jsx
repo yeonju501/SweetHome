223df72df8d1b311ce2934c4143ca14ef663937c
vtaco <woosj7129@naver.com>
2022-02-09 23:01:50 +0900

    [#216] feature: useEffect() 선언

--- frontend/src/components/admin/AdminMemberRegister.jsx
+++ frontend/src/components/admin/AdminMemberRegister.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { useEffect } from "react";
 
 const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 function AdminMemberRegister() {
+	useEffect(() => {}, []);
 	return;
 }
 
