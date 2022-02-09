271073036b8bd4feccdc678580bbbb20c7c4fbbc
9603100 <87457698+9603100@users.noreply.github.com>
2022-02-10 03:23:43 +0900

    [#237] feat: 동의 여부 boolean 값으로 post 하는 함수 구현

--- frontend/src/components/agreements/AgreementDetail.jsx
+++ frontend/src/components/agreements/AgreementDetail.jsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function AgreementDetail() {
 	const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 	const location = useLocation();
+	const navigate = useNavigate();
 	const [agreement, setAgreement] = useState("");
+	const [agreementStatus, setAgreementStatus] = useState("");
 	const agreementId = 1;
+	// const agreementId = location.state.id;
 
 	useEffect(() => {
 		axios({
@@ -17,19 +20,45 @@ function AgreementDetail() {
 		});
 	}, []);
 
+	const handleFormSubmit = (e) => {
+		e.preventDefault();
+		console.log(typeof agreementStatus);
+		axios({
+			url: `${SERVER_URL}/api/agreements/${agreementId}`,
+			method: "post",
+			data: { agreement_status: agreementStatus },
+		})
+			.then((res) => {
+				console.log(res);
+				setAgreementStatus("");
+				navigate("/agreements");
+			})
+			.catch((err) => console.log(err.response));
+	};
+
+	const handleInputChange = (e) => {
+		e.target.value === "agree" ? setAgreementStatus(true) : setAgreementStatus(false);
+	};
+
 	return (
 		<div>
 			<div>
-				<h2>동의서 제목</h2>
+				<h2>{agreement.title}</h2>
 			</div>
 			<article>
-				<p>동의서 내용</p>
+				<p>{agreement.content}</p>
 				<p>몇 동 몇 호 이름</p>
 				<p>오늘 날짜</p>
-				<form>
-					<input type="radio" id="agree" name="status" />
+				<form onSubmit={handleFormSubmit}>
+					<input type="radio" id="agree" name="status" value="agree" onChange={handleInputChange} />
 					<label htmlFor="agree">동의</label>
-					<input type="radio" id="disagree" name="status" />
+					<input
+						type="radio"
+						id="disagree"
+						name="status"
+						value="disagree"
+						onChange={handleInputChange}
+					/>
 					<label htmlFor="disagree">반대</label>
 					<button>제출</button>
 				</form>
