807b31e5b21cf3fcde0320a7d7e5750e7040ced4
9603100 <87457698+9603100@users.noreply.github.com>
2022-02-09 23:25:07 +0900

    [#233] feat: 동의서 게시판 UI 작성

---
+++ frontend/src/components/agreements/Agreements.jsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+
+function Agreements() {
+	return (
+		<div>
+			<h1>관리 동의서 게시판</h1>
+			<table>
+				<thead>
+					<tr>
+						<th>제목</th>
+						<th>기간</th>
+						<th>진행여부</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td></td>
+						<td></td>
+						<td></td>
+					</tr>
+				</tbody>
+			</table>
+			<Link>작성</Link>
+			<footer>페이지네이션</footer>
+		</div>
+	);
+}
+
+export default Agreements;
