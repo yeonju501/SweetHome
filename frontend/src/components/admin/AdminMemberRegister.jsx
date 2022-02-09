3acb5a5c9c1c66884e0ff57120705d8468c8553f
vtaco <woosj7129@naver.com>
2022-02-09 23:59:06 +0900

    [#217] feature: AdminMemberRegister.jsx 임시 디자인

--- frontend/src/components/admin/AdminMemberRegister.jsx
+++ frontend/src/components/admin/AdminMemberRegister.jsx
@@ -7,6 +7,7 @@ const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 function AdminMemberRegister() {
 	const [aptMemberRegister, setAptMemberRegister] = useState({
+		id: "",
 		name: "",
 		email: "",
 		dong: "",
@@ -14,6 +15,7 @@ function AdminMemberRegister() {
 		message: "",
 		phone_number: "",
 	});
+
 	useEffect(() => {
 		console.log("실행");
 		axios({
@@ -31,12 +33,36 @@ function AdminMemberRegister() {
 	}, []);
 	return (
 		<>
-			<h2>회원등록</h2>
-			{aptMemberRegister.length > 0 ? (
-				aptMemberRegister.map((aptMember, idx) => <p>{aptMember.name}</p>)
-			) : (
-				<p>신청자가 없습니다</p>
-			)}
+			<table>
+				<thead>
+					<tr>
+						<th>신청자</th>
+						<th>동</th>
+						<th>호</th>
+						<th>메시지</th>
+						<th></th>
+						<th></th>
+					</tr>
+				</thead>
+				<tbody>
+					{aptMemberRegister.length > 0 ? (
+						aptMemberRegister.map((aptMember, idx) => (
+							<tr key={idx}>
+								<td>{aptMember.name}</td>
+								<td>{aptMember.dong}</td>
+								<td>{aptMember.ho}</td>
+								<td>{aptMember.message}</td>
+								<button>승인</button>
+								<button>거절</button>
+							</tr>
+						))
+					) : (
+						<tr>
+							<td>신청자가 업습니다</td>
+						</tr>
+					)}
+				</tbody>
+			</table>
 		</>
 	);
 }
