9a1338ce946a03548723cfb86923f56d4114277d
IcedCaffeLatte <tmdrl0160@naver.com>
2022-02-09 15:04:06 +0900

    [#223] feat: AptSuperAdminRestController 에 allowAptManager 추가

--- backend/src/main/java/com/sweet/home/apt/controller/AptSuperAdminRestController.java
+++ backend/src/main/java/com/sweet/home/apt/controller/AptSuperAdminRestController.java
@@ -1,10 +1,15 @@
 package com.sweet.home.apt.controller;
 
+import com.sweet.home.apt.controller.dto.request.AptManagerRequest;
 import com.sweet.home.apt.controller.dto.response.RegisterAptManagersResponse;
 import com.sweet.home.apt.service.AptService;
+import java.net.URI;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,9 +23,20 @@ public class AptSuperAdminRestController {
         this.aptService = aptService;
     }
 
-    //등록 요청 - 아파트 등록 요청 회원 목록 보기 = GET
+    // 아파트 관리자 등록 요청 목록보기 = GET
     @GetMapping("/apts/apt-manager")
     public ResponseEntity<RegisterAptManagersResponse> viewAptRegisterManagers(Pageable pageable) {
         return ResponseEntity.ok().body(aptService.viewAptRegisterManagers(pageable));
     }
+
+    // 아파트 관리자 등록 승인하기 = POST
+    @PostMapping("/apts/apt-manager")
+    public ResponseEntity<Void> allowAptManager(@RequestBody AptManagerRequest request) {
+        aptService.allowAptManager(request);
+        URI uri = URI.create("/api/apt-manager/");
+        return ResponseEntity.created(uri).build();
+    }
+
+    // 아파트 관리자 등록 거절하기 = DELETE
+//    @DeleteMapping("/apts/apt-manager")
 }
