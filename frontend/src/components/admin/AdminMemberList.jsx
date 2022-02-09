e7aed8abba4470ed8629fa96a44bcb1400bc7de4
vtaco <woosj7129@naver.com>
2022-02-10 00:30:30 +0900

    [#219] feature: setAptMembers 아파트 회원 조회

--- frontend/src/components/admin/AdminMemberList.jsx
+++ frontend/src/components/admin/AdminMemberList.jsx
@@ -21,12 +21,41 @@ function AdminMemberList() {
 		})
 			.then((res) => {
 				console.log(res.data);
+				setAptMembers(res.data.apt_members);
 			})
 			.catch((err) => {
 				console.log(err);
 			});
 	}, []);
-	return <></>;
+	return (
+		<table>
+			<thead>
+				<tr>
+					<th>신청자</th>
+					<th>동</th>
+					<th>호</th>
+					<th>메시지</th>
+					<th></th>
+				</tr>
+			</thead>
+			<tbody>
+				{aptMembers.length > 0 ? (
+					aptMembers.map((aptMember, idx) => (
+						<tr key={idx}>
+							<td>{aptMember.name}</td>
+							<td>{aptMember.dong}</td>
+							<td>{aptMember.ho}</td>
+							<td>{aptMember.message}</td>
+						</tr>
+					))
+				) : (
+					<tr>
+						<td>신청자가 없습니다</td>
+					</tr>
+				)}
+			</tbody>
+		</table>
+	);
 }
 
 export default AdminMemberList;
