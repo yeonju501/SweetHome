8a4d9210f12461e37ab5d96633f5d4128660e6d6
우동진 <wjinh93@gmail.com>
2022-02-10 12:54:49 +0900

    [#245] feat: 카카오 로그인 성공시, 실패시 명령어 추가

--- frontend/src/components/accounts/AccountKakao.jsx
+++ frontend/src/components/accounts/AccountKakao.jsx
@@ -1,17 +1,28 @@
 import { useEffect } from "react";
 import axios from "axios";
+import { onLoginSuccess } from "../../utils/manageToken";
+import Spinner from "../Spinner";
+
 function AccountKakao() {
 	const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 	useEffect(() => {
 		let params = new URL(document.location.toString()).searchParams;
 		let code = params.get("code");
-
 		axios
 			.get(`${SERVER_URL}/api/oauth2/authorization/kakao?code=${code}`)
-			.catch((err) => console.log(err));
+			.then((res) => {
+				onLoginSuccess(res);
+				window.location.replace("/main");
+			})
+
+			.catch(() => window.location.replace("/error"));
 	}, []);
-	return <div>hi</div>;
+	return (
+		<div>
+			<Spinner />
+		</div>
+	);
 }
 
 export default AccountKakao;
