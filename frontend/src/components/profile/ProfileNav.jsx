71ecd5fbf50d30ec5b364f0da2829f77474c7f12
vtaco <woosj7129@naver.com>
2022-02-10 15:46:56 +0900

    [#250] feature: 아파트 관리자 신청 Nav 추가

--- frontend/src/components/profile/ProfileNav.jsx
+++ frontend/src/components/profile/ProfileNav.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import style from "../../style/Profile.module.css";
 
 function ProfileNav({ active, setActive }) {
-	const arr = ["회원정보", "내가 작성한 글", "내가 작성한 댓글", "내가 좋아요한 글", "아파트 등록"];
+	const arr = [
+		"회원정보",
+		"내가 작성한 글",
+		"내가 작성한 댓글",
+		"내가 좋아요한 글",
+		"아파트 등록",
+		"아파트 관리자 신청",
+	];
 
 	return (
 		<div>
