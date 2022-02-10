a09ac50de16cb2267fe46e94131e5d0d66116dc9
우동진 <wjinh93@gmail.com>
2022-02-10 10:58:09 +0900

    [#241] style: css 추가 적용

--- frontend/src/components/accounts/AccountForgotPassword.jsx
+++ frontend/src/components/accounts/AccountForgotPassword.jsx
@@ -27,12 +27,19 @@ function AccountForgotPassword() {
 	return (
 		<div className={style.sign_in}>
 			<div className={style.sign_in_div}>
+				<h1 className={style.title}>비밀번호 찾기</h1>
+				<p className={style.forgot_pw_p}>
+					아래에 이메일을 입력하시면 <br />
+					임시비밀번호를 입력하신 이메일로 전송합니다
+				</p>
 				<form className={style.form} onSubmit={onSubmit}>
 					<input type="text" placeholder="email" id="email" onChange={onChange} value={email} />
-					<button>이메일로 임시비밀 번호 받기</button>
+					<button className={style.btn_forgot_pw}>이메일로 임시비밀 번호 받기</button>
 				</form>
-				<Link to="/sign-up">회원가입 하러 가기</Link>
-				<Link to="/sign-in">로그인으로 가기</Link>
+				<div className={style.forgot_pw_links}>
+					<Link to="/sign-up">회원가입 하러 가기</Link>
+					<Link to="/sign-in">로그인으로 가기</Link>
+				</div>
 			</div>
 		</div>
 	);
