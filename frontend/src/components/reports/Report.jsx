828656af3470ffd190198358fc3e550a7e63cd2c
우동진 <wjinh93@gmail.com>
2022-02-10 15:00:14 +0900

    [#213] feat: useSelector 사용 및 컴포넌트 분리

--- frontend/src/components/reports/Report.jsx
+++ frontend/src/components/reports/Report.jsx
@@ -1,5 +1,22 @@
+import ReportType from "./ReportType";
+import { useSelector } from "react-redux";
+
 function Report() {
-	return <div>Report</div>;
+	const target = useSelector((state) => state.reportInfo);
+	return (
+		<>
+			<header>
+				<h1>신고하기</h1>
+				<p>
+					내용: <span>{target.content}</span>
+				</p>
+				<p>
+					작성자: <span>{target.username}</span>
+				</p>
+			</header>
+			<ReportType id={target.id} />
+		</>
+	);
 }
 
 export default Report;
--- frontend/src/components/reports/ReportType.jsx
+++ frontend/src/components/reports/ReportType.jsx
@@ -1,12 +1,11 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-function ReportType() {
+function ReportType({ id }) {
 	const URL = process.env.REACT_APP_SERVER_URL;
 	const [reportTypes, setReportTypes] = useState([]);
 	const [types, setType] = useState({ type: "", content: "" });
 
-	const { type } = types;
 	useEffect(() => {
 		axios.get(`${URL}/api/boards/reporttypes`).then((res) => setReportTypes(res.data));
 	});
@@ -18,52 +17,57 @@ function ReportType() {
 		});
 	};
 
+	const { type, content } = types;
+
 	const onSubmit = (e) => {
 		e.preventDefault();
-		setType({ type: "", content: "" });
+		if (!type) return alert("신고 사유를 선택해주세요");
+
+		if (type === "기타" && !content) return alert("신고 사유를 작성해주세요");
+
+		axios({
+			url: `${URL}/api/comments/${id}/reports`,
+			method: "post",
+			data: types,
+		})
+			.then((res) => {
+				setType({ type: "", content: "" });
+				console.log(res);
+			})
+			.catch((err) => console.log(err));
+
 		console.log(types);
 	};
 	return (
 		types && (
-			<div>
-				<header>
-					<h1>신고하기</h1>
-					<p>
-						내용: <span>글 혹은 댓글 내용</span>
-					</p>
-					<p>
-						작성자: <span>작성자 아이디</span>
-					</p>
-				</header>
-				<main>
-					<p>
-						사유선택 : <span>대표적인 사유 1개를 선택해주세요</span>
-					</p>
-					<form onSubmit={onSubmit}>
-						{reportTypes.map((type, idx) => (
-							<div key={idx}>
-								<input
-									name="report"
-									id="type"
-									type="radio"
-									onChange={onChange}
-									value={type.content}
-								/>
-								<label htmlFor="">{type.content}</label>
-							</div>
-						))}
-						{types.type === "기타" && (
-							<textarea
-								id="content"
+			<main>
+				<p>
+					사유선택 : <span>대표적인 사유 1개를 선택해주세요</span>
+				</p>
+				<form onSubmit={onSubmit}>
+					{reportTypes.map((type, idx) => (
+						<div key={idx}>
+							<input
+								name="report"
+								id="type"
+								type="radio"
 								onChange={onChange}
-								placeholder="신고사유를 상세히 적어주세요"
-							></textarea>
-						)}
-						<button>신고하기</button>
-						<button>취소</button>
-					</form>
-				</main>
-			</div>
+								value={type.content}
+							/>
+							<label htmlFor="">{type.content}</label>
+						</div>
+					))}
+					{types.type === "기타" && (
+						<textarea
+							id="content"
+							onChange={onChange}
+							placeholder="신고사유를 상세히 적어주세요"
+						></textarea>
+					)}
+					<button>신고하기</button>
+					<button>취소</button>
+				</form>
+			</main>
 		)
 	);
 }
