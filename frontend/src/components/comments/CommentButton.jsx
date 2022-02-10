4c1d2299a1f80523ff13e8f1207a2f28a7710e6b
우동진 <wjinh93@gmail.com>
2022-02-10 15:31:45 +0900

    [#213] feat: 팝업 및 dispatch

--- frontend/src/components/comments/CommentButton.jsx
+++ frontend/src/components/comments/CommentButton.jsx
@@ -4,7 +4,7 @@ import { SET_REPORT } from "../../store/report";
 import style from "../../style/articles/ArticleDetailComment.module.css";
 import { deleteOrSubmit } from "../../utils/commentAxios";
 
-function CommentButton({ user, comment, activate, onClick, id, getComments }) {
+function CommentButton({ user, comment, onClick, id, getComments }) {
 	const dispatch = useDispatch();
 	const commentDelete = async () => {
 		if (window.confirm("댓글을 삭제 하시겠습니까?")) {
@@ -13,13 +13,13 @@ function CommentButton({ user, comment, activate, onClick, id, getComments }) {
 		}
 	};
 
-	const reportThis = () => {
-		dispatch(SET_REPORT(comment));
-		window.open("/report", "report", "width=430, height=500,location=no,status=no");
+	const reportThis = async () => {
+		await dispatch(SET_REPORT(comment));
+		await window.open("/report", "report", "width=430, height=500,location=no,status=no");
 	};
 	return (
 		<>
-			{user === comment.email && activate ? (
+			{user === comment.email ? (
 				<div className={style.btn_nested_comments}>
 					<button className={style.btn_nested} onClick={onClick}>
 						수정
