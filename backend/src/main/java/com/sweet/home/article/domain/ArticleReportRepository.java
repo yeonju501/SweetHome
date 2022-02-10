0c0c7752a3c8ca1de54826761a7194085a325def
yeonju501 <loling970124@gmail.com>
2022-02-11 02:05:23 +0900

    [#258] feat: findAllByArticleOrderByIdDesc 추가

--- backend/src/main/java/com/sweet/home/article/domain/ArticleReportRepository.java
+++ backend/src/main/java/com/sweet/home/article/domain/ArticleReportRepository.java
@@ -1,6 +1,8 @@
 package com.sweet.home.article.domain;
 
+import com.sweet.home.article.controller.dto.response.ArticleReportDetailResponse;
 import com.sweet.home.member.domain.Member;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -9,6 +11,8 @@ import org.springframework.data.repository.query.Param;
 public interface ArticleReportRepository extends JpaRepository<ArticleReport, Long> {
 
     boolean existsByMemberAndArticle(Member member, Article article);
+
+    List<ArticleReport> findAllByArticleOrderByIdDesc(Article article);
 
     @Modifying(flushAutomatically = true, clearAutomatically = true)
     @Query(nativeQuery = true, value = "update article_report ar set ar.deleted_at = current_timestamp where ar.article_id = (:id) and ar.deleted_at is null")
