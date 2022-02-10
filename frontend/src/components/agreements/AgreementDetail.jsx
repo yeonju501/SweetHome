47cb885f9975784458615689f4ba7fca1c07d1e7
9603100 <87457698+9603100@users.noreply.github.com>
2022-02-10 23:20:39 +0900

    [#268] feat: AgreementDetail에 사용자 이름 표시, 제출 여부에 따라 form 렌더링

--- frontend/src/components/agreements/AgreementDetail.jsx
+++ frontend/src/components/agreements/AgreementDetail.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useLocation, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function AgreementDetail() {
 	const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 	const location = useLocation();
 	const navigate = useNavigate();
+	const username = useSelector((state) => state.userInfo.username);
 	const agreementId = location.state.id;
 	const [agreement, setAgreement] = useState("");
 	const [agreementStatus, setAgreementStatus] = useState("");
@@ -48,30 +50,36 @@ function AgreementDetail() {
 					</div>
 					<article>
 						<p>{agreement.content}</p>
-						<p>몇 동 몇 호 이름</p>
+						<p>몇 동 몇 호 {username}</p>
 						{today && (
 							<p>
 								{today.getFullYear()}년 {today.getMonth() + 1}월 {today.getDate()}일
 							</p>
 						)}
 						<form onSubmit={handleFormSubmit}>
-							<input
-								type="radio"
-								id="agree"
-								name="status"
-								value="agree"
-								onChange={handleInputChange}
-							/>
-							<label htmlFor="agree">동의</label>
-							<input
-								type="radio"
-								id="disagree"
-								name="status"
-								value="disagree"
-								onChange={handleInputChange}
-							/>
-							<label htmlFor="disagree">반대</label>
-							<button>제출</button>
+							{agreement.my_agreed === null ? (
+								<div>
+									<input
+										type="radio"
+										id="agree"
+										name="status"
+										value="agree"
+										onChange={handleInputChange}
+									/>
+									<label htmlFor="agree">동의</label>
+									<input
+										type="radio"
+										id="disagree"
+										name="status"
+										value="disagree"
+										onChange={handleInputChange}
+									/>
+									<label htmlFor="disagree">반대</label>
+									<button>제출</button>
+								</div>
+							) : (
+								<p>이미 제출한 동의서입니다</p>
+							)}
 						</form>
 					</article>
 				</div>
