30989b11c8cecf0b3edf13077c33302518eac465
yeonju501 <loling970124@gmail.com>
2022-02-11 02:04:54 +0900

    [#258] feat: findByIdAndBlockedAtIsNotNull 추가

--- backend/src/main/java/com/sweet/home/article/domain/ArticleRepository.java
+++ backend/src/main/java/com/sweet/home/article/domain/ArticleRepository.java
@@ -4,6 +4,7 @@ import com.sweet.home.board.domain.Board;
 import com.sweet.home.member.domain.Member;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
@@ -24,7 +25,10 @@ public interface ArticleRepository extends JpaRepository<Article, Long> {
     @EntityGraph(attributePaths = {"member"}, type = EntityGraphType.FETCH)
     Page<Article> findAllByBlockedAtIsNotNull(Pageable pageable);
 
+    @EntityGraph(attributePaths = {"board"}, type = EntityGraphType.FETCH)
     List<Article> findByCreatedAtBetweenOrderByTotalLikesDesc(LocalDateTime start, LocalDateTime end, Pageable pageable);
+
+    Optional<Article> findByIdAndBlockedAtIsNotNull(Long id);
 
     @Modifying(flushAutomatically = true, clearAutomatically = true)
     @Query(nativeQuery = true, value = "update article a set a.deleted_at = current_timestamp where a.board_id = (:id) and a.deleted_at is null")
