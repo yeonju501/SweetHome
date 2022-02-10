e922e53dfc82d884699a7012a9ae3a742cd0c92d
vtaco <woosj7129@naver.com>
2022-02-10 14:59:34 +0900

    [#210] style: 멤버 관리 페이지 Nav

--- frontend/src/components/admin/AdminMemberManage.jsx
+++ frontend/src/components/admin/AdminMemberManage.jsx
@@ -1,17 +1,19 @@
 import React from "react";
+import { useState } from "react";
 import AdminMemberList from "./AdminMemberList";
+import AdminMemberNav from "./AdminMemberNav";
 import AdminMemberRegister from "./AdminMemberRegister";
 
 function AdminMemberManage() {
+	const [active, setActive] = useState(0);
 	return (
-		<>
+		<div>
 			<h1>어드민 멤버 관리</h1>
-			<h3>회원등록</h3>
-			<AdminMemberRegister />
-			<hr />
-			<h3>회원조회</h3>
-			<AdminMemberList />
-		</>
+
+			<AdminMemberNav active={active} setActive={setActive} />
+			{active === 0 && <AdminMemberList />}
+			{active === 1 && <AdminMemberRegister />}
+		</div>
 	);
 }
 
