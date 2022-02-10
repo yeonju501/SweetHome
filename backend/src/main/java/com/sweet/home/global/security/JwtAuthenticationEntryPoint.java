dbb97ce2484dfa9bd54285193f4adbcd229109b1
IcedCaffeLatte <tmdrl0160@naver.com>
2022-02-10 13:49:26 +0900

    [#189] feat: JwtAuthenticationEntryPoint 에 ResponseBody 추가

--- backend/src/main/java/com/sweet/home/global/security/JwtAuthenticationEntryPoint.java
+++ backend/src/main/java/com/sweet/home/global/security/JwtAuthenticationEntryPoint.java
@@ -1,5 +1,7 @@
 package com.sweet.home.global.security;
 
+import com.sweet.home.global.exception.ErrorCode;
+import com.sweet.home.global.exception.ErrorResponse;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -19,5 +21,6 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 
         // TODO : ErrorResponse 필드값을 정의한 후 Json 반환
+        response.getWriter().write(ErrorResponse.toJson(ErrorCode.AUTHORITY_ENTRY_POINT));
     }
 }
