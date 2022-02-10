add0a81a95a0157736136380b6e60abc2230b062
vtaco <woosj7129@naver.com>
2022-02-10 15:40:59 +0900

    [#250] feature: 아파트 등록 form

--- frontend/src/components/profile/ProfileAdminEnroll.jsx
+++ frontend/src/components/profile/ProfileAdminEnroll.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 function ProfileAptEnroll() {
-	return;
+	return (
+		<>
+			<form onSubmit={onSubmit}>
+				<label htmlFor="apt_id">아파트 id</label>
+				<input type="text" id="apt_id" />
+				<label htmlFor="message">메시지</label>
+				<input type="text" id="message" />
+
+				<div style={{ display: "flex", alignItems: "center" }}>
+					<button style={{ marginRight: "25rem" }}>변경</button>
+				</div>
+			</form>
+		</>
+	);
 }
 
 export default ProfileAptEnroll;
