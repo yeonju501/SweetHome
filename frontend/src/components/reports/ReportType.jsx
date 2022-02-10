ec397433cf70a3e8976af7cc24e5b188b28ae5a5
우동진 <wjinh93@gmail.com>
2022-02-10 13:44:28 +0900

    [#213] feat: onchange함수및 useState 추가

--- frontend/src/components/reports/ReportType.jsx
+++ frontend/src/components/reports/ReportType.jsx
@@ -3,16 +3,47 @@ import { useEffect, useState } from "react";
 
 function ReportType() {
 	const URL = process.env.REACT_APP_SERVER_URL;
-	const [types, setTypes] = useState([]);
+	const [reortTypes, setReportTypes] = useState([]);
+	const [types, setType] = useState({ type: "", content: "" });
+
+	const { type } = types;
 	useEffect(() => {
-		axios.get(`${URL}/api/boards/reporttypes`).then((res) => setTypes(res.data));
+		axios.get(`${URL}/api/boards/reporttypes`).then((res) => setReportTypes(res.data));
 	});
+
+	const onChange = (e) => {
+		setType({
+			type: e.target.value,
+		});
+	};
 	return (
-		<div>
-			{types.map((type) => (
-				<li>{type.content}</li>
-			))}
-		</div>
+		types && (
+			<div>
+				<header>
+					<h1>신고하기</h1>
+					<p>
+						내용: <span>글 혹은 댓글 내용</span>
+					</p>
+					<p>
+						작성자: <span>작성자 아이디</span>
+					</p>
+				</header>
+				<main>
+					<p>
+						사유선택 : <span>대표적인 사유 1개를 선택해주세요</span>
+					</p>
+					<form>
+						{types.map((type) => (
+							<input type="radio" onChange={onChange} value={type}>
+								{type.content}
+							</input>
+						))}
+						<button>신고하기</button>
+						<button>취소</button>
+					</form>
+				</main>
+			</div>
+		)
 	);
 }
 
