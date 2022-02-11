4c6447e1daaf6ef8b3bf113c43af6eb4cafa2505
vtaco <woosj7129@naver.com>
2022-02-12 02:15:44 +0900

    [#297] feature: AdminBoardUpdate form 생성

--- frontend/src/components/admin/AdminBoardUpdate.jsx
+++ frontend/src/components/admin/AdminBoardUpdate.jsx
@@ -5,7 +5,13 @@ function AdminBoardUpdate(props) {
 	const { isOpen, onCancel, id, name, description } = props;
 	return (
 		<ReactModal isOpen={isOpen}>
-			<h1>hi</h1>
+			<form>
+				<h1>게시판 수정</h1>
+				<input type="text" id="name" value={name} />
+				<input type="text" id="description" value={description} />
+				<button>등록</button>
+				<button onClick={() => onCancel()}>취소</button>
+			</form>
 		</ReactModal>
 	);
 }
