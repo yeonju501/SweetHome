6850e3bd5f86a9d9ff439c239e63a68b804335b5
우동진 <wjinh93@gmail.com>
2022-02-11 22:44:02 +0900

    [#290] feat: 인증 신청 여부에 따라 다른 문구 표시

--- frontend/src/pages/Authority/AssoMemberpage.jsx
+++ frontend/src/pages/Authority/AssoMemberpage.jsx
@@ -1,19 +1,45 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import style from "style/Authority.module.css";
 import { ReactComponent as PersonalInfo } from "assets/personal_info.svg";
-function AssoMemberpage() {
+import axios from "axios";
+
+function AssoMemberpage({}) {
+	const URL = process.env.REACT_APP_SERVER_URL;
+	const [isRequest, setIsRequest] = useState(true);
+	useEffect(() => {
+		axios({
+			url: `${URL}/api/apts/register`,
+			method: "get",
+			headers: { "Content-type": "application/json;charset=UTF-8" },
+		})
+			.then(() => setIsRequest(false))
+			.catch(() => setIsRequest(true));
+	}, []);
 	return (
 		<>
 			<div className={style.asso_div}>
 				<h1>SweetHome을 찾아주셔서 감사합니다</h1>
-				<p>
-					SweetHome은 원활한 서비스 이용을 위해 거주하고 계신 아파트 인증을 필수적으로 진행하고
-					있습니다. <br /> 아래 링크를 통해 꼭 인증을 해주시기 바랍니다
-				</p>
-				<div className={style.Links}>
-					<Link to="/request/apt-member">아파트 세대원 인증하러 가기</Link>
-					<Link to="/request/apt-admin">아파트 관리자 인증하러 가기</Link>
-				</div>
+				{isRequest ? (
+					<div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+						<p>
+							SweetHome은 원활한 서비스 이용을 위해 거주하고 계신 아파트 인증을 필수적으로 진행하고
+							있습니다. <br /> 아래 링크를 통해 꼭 인증을 해주시기 바랍니다
+						</p>
+						<div className={style.Links}>
+							<Link to="/request/apt-member">아파트 세대원 인증하러 가기</Link>
+							<Link to="/request/apt-admin">아파트 관리자 인증하러 가기</Link>
+						</div>
+					</div>
+				) : (
+					<div>
+						<p>관리자가 인증 확인중에 있습니다</p>
+						<form>
+							<button>인증 신청 취소하기</button>
+						</form>
+					</div>
+				)}
+
 				<PersonalInfo />
 			</div>
 		</>
