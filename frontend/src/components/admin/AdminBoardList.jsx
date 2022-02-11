ea36fb420be49f793dd6ea323c652cc2168f1a2a
vtaco <woosj7129@naver.com>
2022-02-12 01:56:02 +0900

    [#288] feature: AdminBoardList.jsx 게시판 리스트 출력

--- frontend/src/components/admin/AdminBoardList.jsx
+++ frontend/src/components/admin/AdminBoardList.jsx
@@ -1,10 +1,16 @@
 import axios from "axios";
 import React from "react";
+import { useState } from "react";
 import { useEffect } from "react";
 
 const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 function AdminBoardList() {
+	const [boardList, setBoardList] = useState({
+		id: "",
+		name: "",
+		description: "",
+	});
 	useEffect(() => {
 		axios({
 			method: "GET",
@@ -12,15 +18,39 @@ function AdminBoardList() {
 		})
 			.then((res) => {
 				console.log(res.data);
+				setBoardList(res.data);
 			})
 			.catch((err) => {
 				console.log(err);
 			});
 	}, []);
 	return (
-		<>
-			<h1>게시판 목록</h1>
-		</>
+		<table>
+			<thead>
+				<tr>
+					<th>이름</th>
+					<th>설명</th>
+					<th></th>
+					<th></th>
+				</tr>
+			</thead>
+			<tbody>
+				{boardList.length > 0 ? (
+					boardList.map((board, idx) => (
+						<tr key={idx}>
+							<td>{board.name}</td>
+							<td>{board.description}</td>
+							<button>수정</button>
+							<button>삭제</button>
+						</tr>
+					))
+				) : (
+					<tr>
+						<td colSpan="4">게시판이 없습니다</td>
+					</tr>
+				)}
+			</tbody>
+		</table>
 	);
 }
 
