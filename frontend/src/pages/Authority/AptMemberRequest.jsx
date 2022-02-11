1f73981388b5009195942714372d3fc4ff8ed3f3
우동진 <wjinh93@gmail.com>
2022-02-11 20:41:25 +0900

    [#283] feat: useState들 추가

--- frontend/src/pages/Authority/AptMemberRequest.jsx
+++ frontend/src/pages/Authority/AptMemberRequest.jsx
@@ -1,4 +1,21 @@
+import axios from "axios";
+import { useState } from "react";
+import DaumPostcode from "react-daum-postcode";
+import style from "style/Authority.module.css";
+
 function AptMemberRequest() {
+	const [addresses, setAddress] = useState({
+		address: "",
+		building: "",
+		unit: "",
+		buildingCode: "",
+		postalCode: "",
+	});
+
+	const [isModal, setIsModal] = useState(false);
+	const { address, building, unit, buildingCode, postalCode } = addresses;
+	const [message, setMessage] = useState("");
+	const URL = process.env.REACT_APP_SERVER_URL;
 	return <div>AptMemberRequest</div>;
 }
 
