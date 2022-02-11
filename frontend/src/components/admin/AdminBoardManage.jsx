c1dbe3eb6bca64ec76d0b04ab9b2c0c201bcfba9
vtaco <woosj7129@naver.com>
2022-02-12 01:43:01 +0900

    [#288] feature: Admin*Manage.jsx Nav 생성

--- frontend/src/components/admin/AdminBoardManage.jsx
+++ frontend/src/components/admin/AdminBoardManage.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import AdminBoardNav from "./AdminBoardNav";
+import AdminBoardRequestList from "./AdminBoardRequestList";
+import AdminBoardList from "./AdminBoardList";
 
 function AdminBoardManage() {
+	const [active, setActive] = useState(0);
 	return (
 		<div>
 			<h1>어드민 게시판 관리</h1>
+
+			<AdminBoardNav active={active} setActive={setActive} />
+			{active === 0 && <AdminBoardList />}
+			{active === 1 && <AdminBoardRequestList />}
 		</div>
 	);
 }
--- frontend/src/components/admin/AdminMemberManage.jsx
+++ frontend/src/components/admin/AdminMemberManage.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import AdminMemberList from "./AdminMemberList";
 import AdminMemberNav from "./AdminMemberNav";
 import AdminMemberRegister from "./AdminMemberRegister";
