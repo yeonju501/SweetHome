1b6350193af1320e185aca72a6a83ae1ec68ace7
vtaco <woosj7129@naver.com>
2022-02-11 17:32:12 +0900

    [#260] feature: AdminBoardList.jsx useEffect(axios) 생성 요청 게시판 조회

--- frontend/src/components/admin/AdminBoardList.jsx
+++ frontend/src/components/admin/AdminBoardList.jsx
@@ -1,7 +1,23 @@
+import axios from "axios";
 import React from "react";
+import { useEffect } from "react";
+
+const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 function AdminBoardList() {
-	return <></>;
+	useEffect(() => {
+		axios({
+			method: "GET",
+			url: `${SERVER_URL}/api/admin/boards`,
+		})
+			.then((res) => {
+				console.log(res.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	});
+	return <h1>어드민 보드 리스트</h1>;
 }
 
 export default AdminBoardList;
