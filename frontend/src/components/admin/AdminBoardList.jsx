ec60d046a9ab99689347e0f3d17d8164c7e7dc71
vtaco <woosj7129@naver.com>
2022-02-11 17:35:06 +0900

    [#260] feature: AdminBoradList.jsx setBoardRequestList res.data.boards 저장

--- frontend/src/components/admin/AdminBoardList.jsx
+++ frontend/src/components/admin/AdminBoardList.jsx
@@ -1,10 +1,16 @@
 import axios from "axios";
 import React from "react";
+import { useState } from "react";
 import { useEffect } from "react";
 
 const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 function AdminBoardList() {
+	const [boardRequestList, setBoardRequestList] = useState({
+		id: "",
+		name: "",
+		description: "",
+	});
 	useEffect(() => {
 		axios({
 			method: "GET",
@@ -12,11 +18,12 @@ function AdminBoardList() {
 		})
 			.then((res) => {
 				console.log(res.data);
+				setBoardRequestList(res.data.boards);
 			})
 			.catch((err) => {
 				console.log(err);
 			});
-	});
+	}, []);
 	return <h1>어드민 보드 리스트</h1>;
 }
 
