0d96550a31596b9647879da54adcb660fa64bc20
vtaco <woosj7129@naver.com>
2022-02-12 02:23:45 +0900

    [#297] feature: onChange() 구현

--- frontend/src/components/admin/AdminBoardUpdate.jsx
+++ frontend/src/components/admin/AdminBoardUpdate.jsx
@@ -9,12 +9,19 @@ function AdminBoardUpdate(props) {
 	});
 	const { isOpen, onCancel, id, name, description } = props;
 
+	function onChange(e) {
+		setBoardUpdate({
+			...boardUpdate,
+			[e.target.id]: e.target.value,
+		});
+	}
+
 	return (
 		<ReactModal isOpen={isOpen}>
+			<h1>게시판 수정</h1>
 			<form>
-				<h1>게시판 수정</h1>
-				<input type="text" id="name" value={name} />
-				<input type="text" id="description" value={description} />
+				<input type="text" id="name" value={name} onChange={onChange} required />
+				<input type="text" id="description" value={description} onChange={onChange} required />
 				<button>등록</button>
 				<button onClick={() => onCancel()}>취소</button>
 			</form>
