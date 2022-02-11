f18069d8f62d8d01dc8980239b9ffe3af11a2018
vtaco <woosj7129@naver.com>
2022-02-11 17:12:24 +0900

    [#260] feature: Navbar.jsx (임시) 게시판 생성 icon

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,6 +5,7 @@ import {
 	faEnvelope,
 	faBars,
 	faHome,
+	faPlus,
 } from "@fortawesome/free-solid-svg-icons";
 import { useSelector, useDispatch } from "react-redux";
 import Cookies from "universal-cookie";
@@ -43,6 +44,8 @@ function Navbar() {
 						</Link>
 					</div>
 					<div className={style.icon_container}>
+						<FontAwesomeIcon className={style.icon} icon={faPlus} />
+
 						<Link to="/message-box/">
 							<FontAwesomeIcon className={style.icon} icon={faEnvelope} />
 						</Link>
