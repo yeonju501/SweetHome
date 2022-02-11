cb70879cef7533e63ccff5eac26eae884e50811a
우동진 <wjinh93@gmail.com>
2022-02-11 20:41:50 +0900

    [#283] feat: 함수들 추가

--- frontend/src/pages/Authority/AptMemberRequest.jsx
+++ frontend/src/pages/Authority/AptMemberRequest.jsx
@@ -16,6 +16,32 @@ function AptMemberRequest() {
 	const { address, building, unit, buildingCode, postalCode } = addresses;
 	const [message, setMessage] = useState("");
 	const URL = process.env.REACT_APP_SERVER_URL;
+
+	const onChange = (e) => {
+		setAddress((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+	};
+
+	const changeMessage = (e) => {
+		setMessage(e.target.value);
+	};
+	const checkNum = /[0-9]/;
+
+	const findAddress = () => {
+		setIsModal((prev) => !prev);
+	};
+
+	const onComplete = (data) => {
+		let addr;
+		data.userSelectedType === "R" ? (addr = data.roadAddress) : (addr = data.jibunAddress);
+		setAddress({
+			...addresses,
+			address: addr,
+			postalCode: data.zonecode,
+			buildingCode: data.buildingCode,
+		});
+		findAddress();
+	};
+
 	return <div>AptMemberRequest</div>;
 }
 
