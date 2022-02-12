e22f611ebefb787b5eeaf161d18f68725a263c83
vtaco <woosj7129@naver.com>
2022-02-13 01:45:25 +0900

    [#308] feature: AdminReportNav.jsx 생성

---
+++ frontend/src/components/admin/AdminReportNav.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import style from "style/Profile.module.css";
+
+function AdminReportNav({ active, setActive }) {
+	const arr = ["신고 게시글 관리", "신고 댓글 관리"];
+
+	return (
+		<div>
+			<ul className={style.nav}>
+				{arr.map((item, idx) => (
+					<li
+						className={active === idx ? style.active : style.deactive}
+						key={idx}
+						onClick={() => setActive(idx)}
+					>
+						{item}
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+}
+
+export default AdminReportNav;
