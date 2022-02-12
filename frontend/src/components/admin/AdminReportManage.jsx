b0a4372c0e598b9c22e526a9f1d25ef9f78b2807
vtaco <woosj7129@naver.com>
2022-02-13 01:48:22 +0900

    [#308] feature: AdminReportManage.jsx Nav 생성

--- frontend/src/components/admin/AdminReportManage.jsx
+++ frontend/src/components/admin/AdminReportManage.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import AdminReportArticleList from "./AdminReportArticleList";
+import AdminReportCommentList from "./AdminReportCommentList";
+import AdminReportNav from "./AdminreportNav";
 
 function AdminReportManage() {
+	const [active, setActive] = useState(0);
 	return (
 		<>
 			<h1>어드민 신고 관리</h1>
-			<AdminReportArticleList />
+			<AdminReportNav active={active} setActive={setActive} />
+			{active === 0 && <AdminReportArticleList />}
+			{active === 0 && <AdminReportCommentList />}
 		</>
 	);
 }
