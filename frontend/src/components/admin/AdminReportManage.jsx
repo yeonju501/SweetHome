e1844d4b5201dfc69466445ce66e75bbe33dcd5b
vtaco <woosj7129@naver.com>
2022-02-13 02:31:30 +0900

    [#308] bugfix: active 수정

--- frontend/src/components/admin/AdminReportManage.jsx
+++ frontend/src/components/admin/AdminReportManage.jsx
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
 import AdminReportArticleList from "./AdminReportArticleList";
 import AdminReportCommentList from "./AdminReportCommentList";
-import AdminReportNav from "./AdminreportNav";
+import AdminReportNav from "./AdminReportNav";
 
 function AdminReportManage() {
 	const [active, setActive] = useState(0);
 	return (
-		<>
+		<div>
 			<h1>어드민 신고 관리</h1>
+
 			<AdminReportNav active={active} setActive={setActive} />
 			{active === 0 && <AdminReportArticleList />}
-			{active === 0 && <AdminReportCommentList />}
-		</>
+			{active === 1 && <AdminReportCommentList />}
+		</div>
 	);
 }
 
