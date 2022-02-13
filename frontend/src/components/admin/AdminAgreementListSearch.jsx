f0770068602b3fec5fdd3f4bd517f01821b2c789
vtaco <woosj7129@naver.com>
2022-02-14 02:03:35 +0900

    [#335] feat: useEffect axios 값 받아오기

--- frontend/src/components/admin/AdminAgreementListSearch.jsx
+++ frontend/src/components/admin/AdminAgreementListSearch.jsx
@@ -1,5 +1,8 @@
-import React from "react";
-import { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+
+const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 function AdminAgreementListSearch() {
 	const [agreementList, setAgreementList] = useState({
@@ -7,6 +10,21 @@ function AdminAgreementListSearch() {
 		ho: "",
 		agreement_status: false,
 		created_at: "",
+	});
+	const location = useLocation();
+	const agreementId = location.state.agreementId;
+
+	useEffect(() => {
+		axios({
+			method: "GET",
+			url: `${SERVER_URL}/api/admin/agreements/agree/${agreementId}`,
+		})
+			.then((res) => {
+				console.log(res.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
 	});
 	return (
 		<>
