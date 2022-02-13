4d50ceba7b359a218790070945ce744d65fd9d82
vtaco <woosj7129@naver.com>
2022-02-14 00:33:16 +0900

    [#332] feat: FontAwesomeIcon, AgreementCreate import & setting

--- frontend/src/components/admin/AdminAgreementManage.jsx
+++ frontend/src/components/admin/AdminAgreementManage.jsx
@@ -1,6 +1,9 @@
 import AgreementCreate from "components/agreements/AgreementCreate";
 import React from "react";
 import { useState } from "react";
+import AgreementCreate from "components/agreements/AgreementCreate";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 function AdimnAgreementManage() {
 	const [modalOpen, setModalOpen] = useState(false);
@@ -11,6 +14,11 @@ function AdimnAgreementManage() {
 	return (
 		<>
 			<h1>어드민 동의서 관리</h1>
+			{modalOpen && <AgreementCreate isOpen={modalOpen} onCancel={handleModal} />}
+			<p>
+				<FontAwesomeIcon onClick={() => setModalOpen(true)} icon={faPlus} />
+				게시판 생성 요청
+			</p>
 		</>
 	);
 }
