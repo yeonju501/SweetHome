49b5e0c46a4f9e7d9015526030c8d402d29e71bc
vtaco <woosj7129@naver.com>
2022-02-14 01:53:00 +0900

    [#335] feat: agreementList useState() 선언

--- frontend/src/components/admin/AdminAgreementListSearch.jsx
+++ frontend/src/components/admin/AdminAgreementListSearch.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { useState } from "react";
 
 function AdminAgreementListSearch() {
+	const [agreementList, setAgreementList] = useState({
+		dong: "",
+		ho: "",
+		agreement_status: false,
+		created_at: "",
+	});
 	return (
 		<>
 			<h1>어드민 동의 리스트 조회</h1>
