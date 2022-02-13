c82862937b753f4500ffb69a1d7a6a34cc8598e0
yeonju501 <loling970124@gmail.com>
2022-02-13 19:21:42 +0900

    [#320] feat: image 삭제 추가

--- backend/src/main/java/com/sweet/home/article/service/ArticleDeleteService.java
+++ backend/src/main/java/com/sweet/home/article/service/ArticleDeleteService.java
@@ -5,9 +5,10 @@ import com.sweet.home.article.domain.ArticleRepository;
 import com.sweet.home.comment.service.CommentDeleteService;
 import com.sweet.home.comment.service.CommentLikeService;
 import com.sweet.home.comment.service.CommentReportService;
-import com.sweet.home.comment.service.CommentService;
 import com.sweet.home.global.exception.BusinessException;
 import com.sweet.home.global.exception.ErrorCode;
+import com.sweet.home.image.ImageUploader;
+import java.util.Objects;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,17 +21,19 @@ public class ArticleDeleteService {
     private final CommentDeleteService commentDeleteService;
     private final CommentLikeService commentLikeService;
     private final CommentReportService commentReportService;
+    private final ImageUploader imageUploader;
 
     public ArticleDeleteService(ArticleRepository articleRepository,
         ArticleLikeService articleLikeService, ArticleReportService articleReportService,
         CommentDeleteService commentDeleteService, CommentLikeService commentLikeService,
-        CommentReportService commentReportService) {
+        CommentReportService commentReportService, ImageUploader imageUploader) {
         this.articleRepository = articleRepository;
         this.articleLikeService = articleLikeService;
         this.articleReportService = articleReportService;
         this.commentDeleteService = commentDeleteService;
         this.commentLikeService = commentLikeService;
         this.commentReportService = commentReportService;
+        this.imageUploader = imageUploader;
     }
 
     @Transactional
@@ -50,6 +53,10 @@ public class ArticleDeleteService {
         Article article = articleRepository.findById(articleId)
             .orElseThrow(() -> new BusinessException(ErrorCode.ARTICLE_NOT_FOUND_BY_ID));
         article.checkArticleByEmail(email);
+
+        if (Objects.nonNull(article.getImageUrl())) {
+            imageUploader.deleteFile(article.getImageUrl());
+        }
         cascadeDeleteArticle(articleId);
     }
 
