28e3b99c1bab0b335e150a05277a327132499528
vtaco <woosj7129@naver.com>
2022-02-14 00:32:03 +0900

    [#332] feat: modalOpen() useState, handleModal 선언

--- frontend/src/components/admin/AdminAgreementManage.jsx
+++ frontend/src/components/admin/AdminAgreementManage.jsx
@@ -1,6 +1,13 @@
+import AgreementCreate from "components/agreements/AgreementCreate";
 import React from "react";
+import { useState } from "react";
 
 function AdimnAgreementManage() {
+	const [modalOpen, setModalOpen] = useState(false);
+
+	const handleModal = () => {
+		setModalOpen(false);
+	};
 	return (
 		<>
 			<h1>어드민 동의서 관리</h1>
