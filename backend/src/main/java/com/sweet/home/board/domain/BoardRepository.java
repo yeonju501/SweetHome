319b4b4d5cd288817b5125a906605efe2fa5e6e2
yeonju501 <loling970124@gmail.com>
2022-02-14 15:52:38 +0900

    [#344] feat: BoardRepository apt 추가

--- backend/src/main/java/com/sweet/home/board/domain/BoardRepository.java
+++ backend/src/main/java/com/sweet/home/board/domain/BoardRepository.java
@@ -1,19 +1,17 @@
 package com.sweet.home.board.domain;
 
+import com.sweet.home.apt.domain.Apt;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
-    Page<Board> findAllByBoardStatusIsNull(Pageable pageable);
+    Page<Board> findAllByBoardStatusIsNullAndApt(Pageable pageable, Apt apt);
 
-    List<Board> findAllByBoardStatusIsNotNull();
+    List<Board> findAllByBoardStatusIsNotNullAndApt(Apt apt);
 
     Optional<Board> findByIdAndBoardStatusIsNull(Long boardId);
 }
