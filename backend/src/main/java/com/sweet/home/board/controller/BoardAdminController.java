c4c91d34c7ee0a3d2160635265c8dcd4b5acd3f2
yeonju501 <loling970124@gmail.com>
2022-02-14 15:50:34 +0900

    [#344] feat: BoardAdminController 아파트 추가

--- backend/src/main/java/com/sweet/home/board/controller/BoardAdminController.java
+++ backend/src/main/java/com/sweet/home/board/controller/BoardAdminController.java
@@ -3,6 +3,7 @@ package com.sweet.home.board.controller;
 import com.sweet.home.board.controller.dto.request.BoardSaveRequest;
 import com.sweet.home.board.controller.dto.response.BoardsResponse;
 import com.sweet.home.board.service.BoardAdminService;
+import com.sweet.home.global.aop.AptChecker;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
@@ -16,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/admin")
+@RequestMapping("/api/apts")
 public class BoardAdminController {
 
     private final BoardAdminService boardAdminService;
@@ -25,37 +26,44 @@ public class BoardAdminController {
         this.boardAdminService = boardAdminService;
     }
 
-    @GetMapping("/boards")
-    public ResponseEntity<BoardsResponse> showUnapprovedBoards(@PageableDefault Pageable pageable) {
-        return ResponseEntity.ok().body(boardAdminService.showUnapprovedBoards(pageable));
+    @AptChecker
+    @GetMapping("/{aptId}/admin/boards")
+    public ResponseEntity<BoardsResponse> showUnapprovedBoards(@PathVariable Long aptId, @PageableDefault Pageable pageable) {
+        return ResponseEntity.ok().body(boardAdminService.showUnapprovedBoards(aptId, pageable));
     }
 
-    @PostMapping("/boards")
-    public ResponseEntity<Void> createBoard(@RequestBody BoardSaveRequest request) {
-        boardAdminService.createBoard(request);
+    @AptChecker
+    @PostMapping("/{aptId}/admin/boards")
+    public ResponseEntity<Void> createBoard(@PathVariable Long aptId, @RequestBody BoardSaveRequest request) {
+        boardAdminService.createBoard(aptId, request);
         return ResponseEntity.noContent().build();
     }
 
-    @PostMapping("/boards/{boardId}/approve")
-    public ResponseEntity<Void> approveBoard(@PathVariable Long boardId) {
+    @AptChecker
+    @PostMapping("/{aptId}/admin/boards/{boardId}/approve")
+    public ResponseEntity<Void> approveBoard(@PathVariable Long aptId, @PathVariable Long boardId) {
         boardAdminService.approveBoard(boardId);
         return ResponseEntity.noContent().build();
     }
 
-    @DeleteMapping("/boards/{boardId}/approve")
-    public ResponseEntity<Void> disapproveBoard(@PathVariable Long boardId) {
+    @AptChecker
+    @DeleteMapping("/{aptId}/admin/boards/{boardId}/approve")
+    public ResponseEntity<Void> disapproveBoard(@PathVariable Long aptId, @PathVariable Long boardId) {
         boardAdminService.disapproveBoard(boardId);
         return ResponseEntity.noContent().build();
     }
 
-    @PutMapping("/boards/{boardId}")
-    public ResponseEntity<Void> updateBoard(@PathVariable Long boardId, @RequestBody BoardSaveRequest boardSaveRequest) {
+    @AptChecker
+    @PutMapping("/{aptId}/admin/boards/{boardId}")
+    public ResponseEntity<Void> updateBoard(@PathVariable Long aptId, @PathVariable Long boardId,
+        @RequestBody BoardSaveRequest boardSaveRequest) {
         boardAdminService.updateBoard(boardId, boardSaveRequest);
         return ResponseEntity.noContent().build();
     }
 
-    @DeleteMapping("/boards/{boardId}")
-    public ResponseEntity<Void> deleteBoard(@PathVariable Long boardId) {
+    @AptChecker
+    @DeleteMapping("/{aptId}/admin/boards/{boardId}")
+    public ResponseEntity<Void> deleteBoard(@PathVariable Long aptId, @PathVariable Long boardId) {
         boardAdminService.deleteBoard(boardId);
         return ResponseEntity.noContent().build();
     }
