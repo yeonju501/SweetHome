873f130da1a8389ae0e78ca7e2b116359b378fdf
yeonju501 <loling970124@gmail.com>
2022-02-14 22:40:10 +0900

    [#358] feat: Agreement Lob 설정 추가

--- backend/src/main/java/com/sweet/home/agreement/domain/Agreement.java
+++ backend/src/main/java/com/sweet/home/agreement/domain/Agreement.java
@@ -14,6 +14,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import lombok.Builder;
 import lombok.Getter;
@@ -37,6 +38,7 @@ public class Agreement extends BaseEntity {
     @Column(name = "title", length = 100, nullable = false)
     private String title;
 
+    @Lob
     @Column(name = "content", length = 400, nullable = false)
     private String content;
 
