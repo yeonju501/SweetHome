81fbd27e3bf07a3ebe07da774f811ad6d9052855
vtaco <woosj7129@naver.com>
2022-02-14 16:57:21 +0900

    [#347] refactor: AdminBoardrequestList.jsx Pagenation

--- frontend/src/components/admin/board/AdminBoardRequestList.jsx
+++ frontend/src/components/admin/board/AdminBoardRequestList.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
+import { adminPagination, pageDown, pageUp } from "utils/adminFunction";
 
 const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
@@ -11,6 +12,8 @@ function AdminBoardRequestList() {
 		name: "",
 		description: "",
 	});
+	const [page, setPage] = useState(0);
+	const [pageSize, setPageSize] = useState(0);
 
 	useEffect(() => {
 		axios({
@@ -19,11 +22,12 @@ function AdminBoardRequestList() {
 		})
 			.then((res) => {
 				setBoardRequestList(res.data.boards);
+				setPageSize(res.data.total_page_count);
 			})
 			.catch((err) => {
 				console.log(err);
 			});
-	}, []);
+	}, [page]);
 
 	const approveBoard = (method, id) => {
 		axios({
@@ -34,44 +38,53 @@ function AdminBoardRequestList() {
 		});
 	};
 	return (
-		<table>
-			<thead>
-				<tr>
-					<th>신청자</th>
-					<th>내용</th>
-					<th></th>
-					<th></th>
-				</tr>
-			</thead>
-			<tbody>
-				{boardRequestList.length > 0 ? (
-					boardRequestList.map((boardRequest, idx) => (
-						<tr key={idx}>
-							<td>{boardRequest.name}</td>
-							<td>{boardRequest.description}</td>
-							<button
-								onClick={(e) => {
-									approveBoard("POST", boardRequest.id);
-								}}
-							>
-								승인
-							</button>
-							<button
-								onClick={(e) => {
-									approveBoard("DELETE", boardRequest.id);
-								}}
-							>
-								거절
-							</button>
-						</tr>
-					))
-				) : (
+		<>
+			<table>
+				<thead>
 					<tr>
-						<td colSpan="4">신청 게시판이 없습니다</td>
+						<th>신청자</th>
+						<th>내용</th>
+						<th></th>
+						<th></th>
 					</tr>
-				)}
-			</tbody>
-		</table>
+				</thead>
+				<tbody>
+					{boardRequestList.length > 0 ? (
+						boardRequestList.map((boardRequest, idx) => (
+							<tr key={idx}>
+								<td>{boardRequest.name}</td>
+								<td>{boardRequest.description}</td>
+								<button
+									onClick={(e) => {
+										approveBoard("POST", boardRequest.id);
+									}}
+								>
+									승인
+								</button>
+								<button
+									onClick={(e) => {
+										approveBoard("DELETE", boardRequest.id);
+									}}
+								>
+									거절
+								</button>
+							</tr>
+						))
+					) : (
+						<tr>
+							<td colSpan="4">신청 게시판이 없습니다</td>
+						</tr>
+					)}
+				</tbody>
+			</table>
+			{boardRequestList.length > 0 ? (
+				<div>
+					<button onClick={() => pageDown(page, pageSize, setPage)}>&lt;</button>
+					{adminPagination(pageSize, setPage)}
+					<button onClick={() => pageUp(page, pageSize, setPage)}>&gt;</button>
+				</div>
+			) : null}
+		</>
 	);
 }
 
