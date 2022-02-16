042b6ad1e8e61386880749c8b3a4330fa11fe513
우동진 <wjinh93@gmail.com>
2022-02-16 16:14:07 +0900

    [#404] style: 500px  반응형 수정

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -3,16 +3,18 @@ import { Outlet } from "react-router";
 import { checkSidebar, checkUseNav } from "utils/authority";
 import Banner from "components/Banner";
 import style from "style/App.module.css";
-import Admin from "pages/Admin";
 
 function Layout() {
 	const authority = useSelector((state) => state.userInfo.authority);
+	const toggle = useSelector((state) => state.toggle.toggleValue);
 
 	return (
 		<>
 			{checkUseNav(authority)}
 			<div className={authority ? style.div : style.public}>
-				{checkSidebar(authority)}
+				<aside className={(style.aside, toggle ? style.show : style.unshow)}>
+					{checkSidebar(authority)}
+				</aside>
 				<main className={authority ? style.main : style.public}>
 					<Outlet />
 				</main>
