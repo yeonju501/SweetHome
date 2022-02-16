c822a7d12013bc52f045355118532e8afdc7eb5f
yeonju501 <loling970124@gmail.com>
2022-02-16 15:49:33 +0900

    [#400] feat: CommentLikeRestController AptChecker 추가

--- backend/src/main/java/com/sweet/home/comment/controller/CommentLikeRestController.java
+++ backend/src/main/java/com/sweet/home/comment/controller/CommentLikeRestController.java
@@ -2,9 +2,9 @@ package com.sweet.home.comment.controller;
 
 import com.sweet.home.article.controller.dto.response.LikeStatusResponse;
 import com.sweet.home.comment.service.CommentLikeService;
+import com.sweet.home.global.aop.AptChecker;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,17 +22,20 @@ public class CommentLikeRestController {
         this.commentLikeService = commentLikeService;
     }
 
+    @AptChecker
     @PostMapping("/{aptId}/comments/{commentId}/likes")
     public ResponseEntity<Void> likeComment(@AuthenticationPrincipal String email, @PathVariable Long aptId, @PathVariable Long commentId) {
         commentLikeService.likeComment(email, commentId);
         return ResponseEntity.noContent().build();
     }
 
+    @AptChecker
     @GetMapping("/{aptId}/comments/{commentId}/likes")
     public ResponseEntity<LikeStatusResponse> showCommentLikeStatus(@AuthenticationPrincipal String email, @PathVariable Long aptId, @PathVariable Long commentId){
         return ResponseEntity.ok().body(new LikeStatusResponse(commentLikeService.showCommentLikeStatus(email, commentId)));
     }
 
+    @AptChecker
     @DeleteMapping("/{aptId}/comments/{commentId}/likes")
     public ResponseEntity<Void> deleteLike(@AuthenticationPrincipal String email, @PathVariable Long aptId, @PathVariable Long commentId) {
         commentLikeService.deleteLike(email, commentId);
