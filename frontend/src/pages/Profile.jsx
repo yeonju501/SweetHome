e2576bfa67113cf3a93f089325ed351d82aa844a
우동진 <wjinh93@gmail.com>
2022-02-17 14:16:29 +0900

    [#447] style: 이사가기 가운데 정렬

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -35,7 +35,11 @@ function Profile() {
 			{active === 1 && <ProfileArticles />}
 			{active === 2 && <ProfileComments />}
 			{active === 3 && <ProfileLikes />}
-			{active === 4 && <AptMemberRequest moving="이사가기" />}
+			{active === 4 && (
+				<div style={{ margin: "2rem auto" }}>
+					<AptMemberRequest moving="이사가기" />
+				</div>
+			)}
 		</div>
 	);
 }
