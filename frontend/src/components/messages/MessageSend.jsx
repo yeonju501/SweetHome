2c9f3c47551b46222711cf5a71eb72f579e74448
우동진 <wjinh93@gmail.com>
2022-02-19 16:36:42 +0900

    [#481] refactor: 쓸모 없는 코드 삭제

--- frontend/src/components/messages/MessageSend.jsx
+++ frontend/src/components/messages/MessageSend.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import { toast } from "react-toastify";
 import style from "style/Messages.module.css";
 import errorMessage from "store/errorMessage";
 
