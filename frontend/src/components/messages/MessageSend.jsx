eb113fdb872d1b1e044fbeb62785751400f40d16
우동진 <wjinh93@gmail.com>
2022-02-19 16:32:19 +0900

    [#481] refactor: target 추가

--- frontend/src/components/messages/MessageSend.jsx
+++ frontend/src/components/messages/MessageSend.jsx
@@ -1,15 +1,19 @@
-import React, { useState } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { toast } from "react-toastify";
 import style from "style/Messages.module.css";
 import errorMessage from "store/errorMessage";
 
-function SendMessage() {
+function SendMessage({ target }) {
 	const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 	const navigate = useNavigate();
-	const location = useLocation();
 
+	useEffect(() => {
+		if (target) {
+			setSendMessage({ ...sendMessage, receiver_name: target });
+		}
+	}, []);
 	const [sendMessage, setSendMessage] = useState({
 		receiver_name: "",
 		title: "",
@@ -25,16 +29,20 @@ function SendMessage() {
 
 	function onSend(e) {
 		e.preventDefault();
-
-		if (receiver_name.trim() && title.trim() && content.trim()) {
+		if ((receiver_name.trim() || target) && title.trim() && content.trim()) {
 			axios({
 				method: "POST",
 				url: `${SERVER_URL}/api/messages/`,
 				data: sendMessage,
 			})
 				.then(() => {
-					toast.success("메시지 전송 완료");
+					alert("메시지 전송 완료");
 					setSendMessage({ receiver_name: "", title: "", content: "" });
+				})
+				.then(() => {
+					if (target) {
+						window.close();
+					}
 				})
 				.catch((err) => {
 					errorMessage(err.response.data.error_code);
@@ -45,6 +53,9 @@ function SendMessage() {
 	}
 
 	function onCancel() {
+		if (target) {
+			window.close();
+		}
 		navigate("/message-box");
 	}
 
@@ -56,15 +67,19 @@ function SendMessage() {
 			<form className={style.send_form}>
 				<div className={style.message_to}>
 					<label className={style.label}>받는 사람</label>
-					<input
-						autoFocus="autofocus"
-						className={style.input_box}
-						type="text"
-						id="receiver_name"
-						value={receiver_name}
-						onChange={onChange}
-						required
-					/>
+					{target ? (
+						<input className={style.input_box} type="text" value={target} readOnly />
+					) : (
+						<input
+							autoFocus="autofocus"
+							className={style.input_box}
+							type="text"
+							id="receiver_name"
+							value={receiver_name}
+							onChange={onChange}
+							required
+						/>
+					)}
 				</div>
 				<div className={style.message_title}>
 					<label className={style.label}>제목</label>
